f493420e7e78876208bc1ea87a52a8b3d106aefd
Stéphane Gaudreault <stephane.gaudreault2@canada.ca>
2020-01-27 14:16:39 -0500

    rhs

--- rhs_fun.py
+++ rhs_fun.py
@@ -6,8 +6,8 @@ def rhs_fun(Q, geom, mtrx, metric, nbsolpts, nb_elements_x1, nb_elements_x2, α)
 
    type_vec = type(Q[1,1,1])
 
-   nb_interfaces_x1 = nb_elements_x1 - 1
-   nb_interfaces_x2 = nb_elements_x2 - 1
+   nb_interfaces_x1 = nb_elements_x1 + 1
+   nb_interfaces_x2 = nb_elements_x2 + 1
 
    flux_x1          = numpy.zeros_like(Q, dtype=type_vec)
    flux_x2          = numpy.zeros_like(Q, dtype=type_vec)
@@ -20,21 +20,51 @@ def rhs_fun(Q, geom, mtrx, metric, nbsolpts, nb_elements_x1, nb_elements_x2, α)
    Q_yy             = numpy.zeros_like(Q, dtype=type_vec)
    upwind_diffusion = numpy.zeros_like(Q, dtype=type_vec)
 
-   jfaces_flux      = numpy.zeros((2, nbsolpts*nb_elements_x1, nb_equations, nb_elements_x2), dtype=type_vec)
-   jfaces_var       = numpy.zeros((2, nbsolpts*nb_elements_x1, nb_equations, nb_elements_x2), dtype=type_vec)
-   jfaces_diffusion = numpy.zeros((2, nbsolpts*nb_elements_x1, nb_equations, nb_elements_x2), dtype=type_vec)
+   jfaces_flux      = numpy.zeros((2, nbsolpts*nb_elements_x1, nb_equations, nb_interfaces_x2), dtype=type_vec)
+   jfaces_var       = numpy.zeros((2, nbsolpts*nb_elements_x1, nb_equations, nb_interfaces_x2), dtype=type_vec)
+   jfaces_diffusion = numpy.zeros((2, nbsolpts*nb_elements_x1, nb_equations, nb_interfaces_x2), dtype=type_vec)
 
-   ifaces_flux      = numpy.zeros((nbsolpts*nb_elements_x2, 2, nb_equations, nb_elements_x1), dtype=type_vec)
-   ifaces_var       = numpy.zeros((nbsolpts*nb_elements_x2, 2, nb_equations, nb_elements_x1), dtype=type_vec)
-   ifaces_diffusion = numpy.zeros((nbsolpts*nb_elements_x2, 2, nb_equations, nb_elements_x1), dtype=type_vec)
-
-   jfaces_pres = numpy.zeros((2, nbsolpts*nb_elements_x1, nb_elements_x2), dtype=type_vec)
-   ifaces_pres = numpy.zeros((nbsolpts*nb_elements_x2, 2, nb_elements_x1), dtype=type_vec)
+   ifaces_flux      = numpy.zeros((nbsolpts*nb_elements_x2, 2, nb_equations, nb_interfaces_x1), dtype=type_vec)
+   ifaces_var       = numpy.zeros((nbsolpts*nb_elements_x2, 2, nb_equations, nb_interfaces_x1), dtype=type_vec)
+   ifaces_diffusion = numpy.zeros((nbsolpts*nb_elements_x2, 2, nb_equations, nb_interfaces_x1), dtype=type_vec)
 
    # Unpack physical variables
    h  = Q[:,:,0]
    u1 = Q[:,:,1] / Q[:,:,0]
    u2 = Q[:,:,2] / Q[:,:,0]
+   # TODO : indices des variables
+
+   # Interpolate to the element interface
+   for elem in range(nb_elements_x2):
+      epais = elem * nbsolpts + numpy.arange(nbsolpts)
+      itf_l = elem
+      itf_r = elem + 1
+ 
+      jfaces_var[1,:,0,itf_l] = mtrx.lcoef @ h[epais,:]
+      jfaces_var[0,:,0,itf_r] = mtrx.rcoef @ h[epais,:]
+      
+      jfaces_var[1,:,1,itf_l] = mtrx.lcoef @ u1[epais,:]
+      jfaces_var[0,:,1,itf_r] = mtrx.rcoef @ u1[epais,:]
+
+      jfaces_var[1,:,2,itf_l] = mtrx.lcoef @ u2[epais,:]
+      jfaces_var[0,:,2,itf_r] = mtrx.rcoef @ u2[epais,:]
+
+   for elem in range(nb_elements_x1):
+      epais = elem * nbsolpts + numpy.arange(nbsolpts)
+      itf_l = elem
+      itf_r = elem + 1
+
+      ifaces_var[:,1,0,itf_l] = h[:,epais] @ mtrx.lcoef
+      ifaces_var[:,0,0,itf_r] = h[:,epais] @ mtrx.rcoef
+
+      ifaces_var[:,1,0,itf_l] = u1[:,epais] @ mtrx.lcoef
+      ifaces_var[:,0,0,itf_r] = u1[:,epais] @ mtrx.rcoef
+
+      ifaces_var[:,1,0,itf_l] = u2[:,epais] @ mtrx.lcoef
+      ifaces_var[:,0,0,itf_r] = u2[:,epais] @ mtrx.rcoef
+
+   exit(0)
+   xchange_halo(ifaces_var, jfaces_var)
 
    # Compute the fluxes
    flux_x1[:,:,0] = Q[:,:,1]
@@ -44,50 +74,6 @@ def rhs_fun(Q, geom, mtrx, metric, nbsolpts, nb_elements_x1, nb_elements_x2, α)
    flux_x2[:,:,0] = Q[:,:,2]
    flux_x2[:,:,1] = Q[:,:,2] * u1 + 0.5 * metric.H_contra_21 * gravity * Q[:,:,0]**2
    flux_x2[:,:,2] = Q[:,:,2] * u2 + 0.5 * metric.H_contra_22 * gravity * Q[:,:,0]**2
-
-   # Interpolate to the element interface
-   for elem in range(nb_elements_x2):
-      epais = elem * nbsolpts + numpy.arange(nbsolpts)
-
-      for eq in range(nb_equations):
-         jfaces_flux[0,:,eq,elem] = mtrx.lcoef @ flux_x2[epais,:,eq]
-         jfaces_flux[1,:,eq,elem] = mtrx.rcoef @ flux_x2[epais,:,eq]
-
-         jfaces_var[0,:,eq,elem] = mtrx.lcoef @ Q[epais,:,eq]
-         jfaces_var[1,:,eq,elem] = mtrx.rcoef @ Q[epais,:,eq]
-
-   for elem in range(nb_elements_x1):
-      epais = elem * nbsolpts + numpy.arange(nbsolpts)
-
-      for eq in range(nb_equations):
-         ifaces_flux[:,0,eq,elem] = flux_x1[:,epais,eq] @ mtrx.lcoef
-         ifaces_flux[:,1,eq,elem] = flux_x1[:,epais,eq] @ mtrx.rcoef
-
-         ifaces_var[:,0,eq,elem] = Q[:,epais,eq] @ mtrx.lcoef
-         ifaces_var[:,1,eq,elem] = Q[:,epais,eq] @ mtrx.rcoef
-
-   print('TODO : halo exchange');exit(0)
-
-   # Bondary treatement
-   # zeros flux BCs everywhere ...
-   jfaces_flux[0,:,:,0]  = 0.0
-   jfaces_flux[1,:,:,-1] = 0.0
-
-   ifaces_flux[:,0,:,0]  = 0.0
-   ifaces_flux[:,1,:,-1] = 0.0
-
-   jfaces_diffusion[0,:,:,0]  = 0.0
-   jfaces_diffusion[1,:,:,-1] = 0.0
-
-   ifaces_diffusion[:,0,:,0]  = 0.0
-   ifaces_diffusion[:,1,:,-1] = 0.0
-
-   # ... except for momentum eqs where pressure is extrapolated to BCs.
-   jfaces_flux[0,:,RHO_W,0]  = jfaces_pres[0,:,0]
-   jfaces_flux[1,:,RHO_W,-1] = jfaces_pres[1,:,-1]
-
-   ifaces_flux[:,0,RHO_U,0]   = ifaces_pres[:,0,0]  # TODO : theo seulement ...
-   ifaces_flux[:,1,RHO_U,-1] = ifaces_pres[:,1,-1]
 
    # Common Rusanov fluxes
    for itf in range(nb_interfaces_x2):
@@ -168,7 +154,7 @@ def rhs_fun(Q, geom, mtrx, metric, nbsolpts, nb_elements_x1, nb_elements_x2, α)
 
    ifaces_diffusion[:,0,:,0]  = 0.0
    ifaces_diffusion[:,1,:,-1] = 0.0
-#
+
    # Finalize the diffusion operator
    for elem in range(nb_elements_x2):
       epais = elem * nbsolpts + numpy.arange(nbsolpts)
