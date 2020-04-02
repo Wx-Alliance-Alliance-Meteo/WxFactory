1bf689f08a2130ec12fa179dd31d54970115483d
Stéphane Gaudreault <stephane.gaudreault2@canada.ca>
2020-04-02 12:52:11 -0400

    physical constants

--- definitions.py
+++ definitions.py
@@ -9,6 +9,13 @@ rotation_speed = 7.29212e-5     # Angular speed of rotation of the earth (radian
 
 inv_earth_radius = 1.0 / earth_radius
 
+p0  = 100000.     # reference pressure (Pa)
+Rd  = 287.05  # J K-1 kg-1 ! gas constant for dry air
+cpd = 1005.46
+cvd = (cpd - Rd)  # chal. spec. air sec (volume constant) [J kg-1 K-1]
+
+heat_capacity_ratio = cpd / cvd
+
 # TODO : reviser
 idx_h   = 0
 idx_hu1 = 1
