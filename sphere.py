4e3a8f25ca67cfdf887710bb12eaf3175c57bbe0
Stéphane Gaudreault <stephane.gaudreault2@canada.ca>
2020-07-13 16:14:13 -0400

    Map longitude to the interval [0, 2 pi]

--- sphere.py
+++ sphere.py
@@ -1,8 +1,9 @@
 import numpy
+import math
 
 def sph2cart(az, elev, radius):
-   """ SPH2CART Transform spherical to Cartesian coordinates.
-      [X,Y,Z] = SPH2CART(TH,PHI,radius) transforms corresponding elements of
+   """ Transform spherical to Cartesian coordinates.
+      [X,Y,Z] = sph2cart(TH,PHI,radius) transforms corresponding elements of
       data stored in spherical coordinates (azimuth TH, elevation PHI,
       radius radius) to Cartesian coordinates X,Y,Z.  The arrays TH, PHI, and
       radius must be the same size (or any of them can be scalar).  TH and
@@ -20,8 +21,8 @@ def sph2cart(az, elev, radius):
    return x, y, z
 
 def cart2sph(x,y,z):
-   """ CART2SPH Transform Cartesian to spherical coordinates.
-      az,elev,r = CART2SPH(X,Y,Z) transforms corresponding elements of
+   """ Transform Cartesian to spherical coordinates.
+      az,elev,r = cart2sph(X,Y,Z) transforms corresponding elements of
       data stored in Cartesian coordinates X,Y,Z to spherical
       coordinates (azimuth, elevation, and radius).  The arrays
       X,Y, and Z must be the same size (or any of them can be scalar).
@@ -35,5 +36,8 @@ def cart2sph(x,y,z):
    r       = numpy.hypot(hypotxy, z)
    elev    = numpy.arctan2(z, hypotxy)
    az      = numpy.arctan2(y, x)
+
+   # Map to the interval [0, 2 pi]
+   az[az<0.] += 2. * math.pi
 
    return az, elev, r
