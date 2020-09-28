bab503f92c94d4a890a82017612f9f18338292c2
Stéphane Gaudreault <stephane.gaudreault2@canada.ca>
2020-09-28 18:15:18 -0400

    Fix for the formulation of the total energy

--- diagnostic.py
+++ diagnostic.py
@@ -42,15 +42,15 @@ def total_energy(h, u1_contra, u2_contra, geom, topo, metric):
    u2_dual = metric.H_cov_21 * u1_contra + metric.H_cov_22 * u2_contra
 
    # Kinetic energy
-   kinetic = 0.5 * (u1_dual * u1_contra + u2_dual * u2_contra)
+   kinetic = 0.5 * h * (u1_dual * u1_contra + u2_dual * u2_contra)
 
    # Potential energy
-   potential = gravity * (h + topo.hsurf)
+   potential = 0.5 * gravity * ( (h + topo.hsurf)**2 - topo.hsurf**2 )
 
    # "Total" energy
    return kinetic + potential
 
 def potential_enstrophy(h, u1_contra, u2_contra, geom, metric, mtrx, param):
    rv = relative_vorticity(u1_contra, u2_contra, geom, metric, mtrx, param)
-   return 0.5 * (rv + metric.coriolis_f)**2 / h
+   return (rv + metric.coriolis_f)**2 / (2 * h)
 
