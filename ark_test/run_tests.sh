fb798d17f9635a355152ef153c37c49ad493a865
Vincent Magnoux <vmagnoux20@gmail.com>
2020-11-24 16:05:19 -0500

    Simplified test script. Better test case parameters

--- ark_test/run_tests.sh
+++ ark_test/run_tests.sh
@@ -5,9 +5,12 @@ case_file="test_case.ini"
 RUN="/usr/bin/mpirun -H 127.0.0.1:6 -merge-stderr-to-stdout -tag-output -n 6 -cpu-list 0,1,1,2,2,3 python3 ../main_gef.py ${case_file}"
 
 SOL_PT_COUNTS="4 5"
-ELEM_COUNTS="10 25"
+ELEM_COUNTS="15 30"
 TIME_STEPS="450 1800"
 
+SCHEMES_BOTH="ARK4(3)7L[2]SA ARK5(4)8L[2]SA ARK5(4)8L[2]SAb ARK3(2)4L[2]SA"
+SCHEMES_IMP="ESDIRK3(2)5L[2]SA ESDIRK3(2I)5L[2]SA ESDIRK4(3)6L[2]SA ESDIRK4(3I)6L[2]SA ESDIRK4(3I)6L[2]SA QESDIRK4(3)6L[2]SA ESDIRK5(3)6L[2]SA ESDIRK5(4)7L[2]SA"
+
 #for nb_sol_pt in ${SOL_PT_COUNTS}; do
 #    for nb_elem in ${ELEM_COUNTS}; do
 #        for dt in ${TIME_STEPS}; do
@@ -20,158 +23,36 @@ TIME_STEPS="450 1800"
 #done
 #
 
-#sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp=ARK4(3)7L[2]SA-ERK/' \
-#                    -e 's/^ark_solver_imp=.*/ark_solver_imp=ARK4(3)7L[2]SA-ESDIRK/' \
+
+#for scheme in ${SCHEMES_BOTH}; do
+#    sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp='${scheme}'-ERK/' \
+#                        -e 's/^ark_solver_imp=.*/ark_solver_imp='${scheme}'-ESDIRK/' \
 #
-#for nb_sol_pt in ${SOL_PT_COUNTS}; do
-#    for nb_elem in ${ELEM_COUNTS}; do
-#        for dt in ${TIME_STEPS}; do
-#            sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
-#                                -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
-#                                -e 's/^dt=.*/dt='${dt}'/'
-#            ${RUN}
+#    for nb_sol_pt in ${SOL_PT_COUNTS}; do
+#        for nb_elem in ${ELEM_COUNTS}; do
+#            for dt in ${TIME_STEPS}; do
+#                sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
+#                                    -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
+#                                    -e 's/^dt=.*/dt='${dt}'/'
+#                ${RUN}
+#            done
 #        done
 #    done
 #done
 
-#sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp=ARK5(4)8L[2]SA-ERK/' \
-#                    -e 's/^ark_solver_imp=.*/ark_solver_imp=ARK5(4)8L[2]SA-ESDIRK/' \
-#
-#for nb_sol_pt in ${SOL_PT_COUNTS}; do
-#    for nb_elem in ${ELEM_COUNTS}; do
-#        for dt in ${TIME_STEPS}; do
-#            sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
-#                                -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
-#                                -e 's/^dt=.*/dt='${dt}'/'
-#            ${RUN}
-#        done
-#    done
-#done
-#
-#sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp=ARK5(4)8L[2]SAb-ERK/' \
-#                    -e 's/^ark_solver_imp=.*/ark_solver_imp=ARK5(4)8L[2]SAb-ESDIRK/' \
-#
-#for nb_sol_pt in ${SOL_PT_COUNTS}; do
-#    for nb_elem in ${ELEM_COUNTS}; do
-#        for dt in ${TIME_STEPS}; do
-#            sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
-#                                -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
-#                                -e 's/^dt=.*/dt='${dt}'/'
-#            ${RUN}
-#        done
-#    done
-#done
+for scheme in ${SCHEMES_IMP}; do
+    sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp='${scheme}'/' \
+                        -e 's/^ark_solver_imp=.*/ark_solver_imp='${scheme}'/' \
 
-#sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp=ARK3(2)4L[2]SA-ERK/' \
-#                    -e 's/^ark_solver_imp=.*/ark_solver_imp=ARK3(2)4L[2]SA-ESDIRK/' \
-#
-#for nb_sol_pt in ${SOL_PT_COUNTS}; do
-#    for nb_elem in ${ELEM_COUNTS}; do
-#        for dt in ${TIME_STEPS}; do
-#            sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
-#                                -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
-#                                -e 's/^dt=.*/dt='${dt}'/'
-#            ${RUN}
-#        done
-#    done
-#done
-
-#sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp=ESDIRK3(2)5L[2]SA/' \
-#                    -e 's/^ark_solver_imp=.*/ark_solver_imp=ESDIRK3(2)5L[2]SA/' \
-#
-#for nb_sol_pt in ${SOL_PT_COUNTS}; do
-#    for nb_elem in ${ELEM_COUNTS}; do
-#        for dt in ${TIME_STEPS}; do
-#            sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
-#                                -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
-#                                -e 's/^dt=.*/dt='${dt}'/'
-#            ${RUN}
-#        done
-#    done
-#done
-#
-#sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp=ESDIRK3(2I)5L[2]SA/' \
-#                    -e 's/^ark_solver_imp=.*/ark_solver_imp=ESDIRK3(2I)5L[2]SA/' \
-#
-#for nb_sol_pt in ${SOL_PT_COUNTS}; do
-#    for nb_elem in ${ELEM_COUNTS}; do
-#        for dt in ${TIME_STEPS}; do
-#            sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
-#                                -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
-#                                -e 's/^dt=.*/dt='${dt}'/'
-#            ${RUN}
-#        done
-#    done
-#done
-#
-#sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp=ESDIRK4(3)6L[2]SA/' \
-#                    -e 's/^ark_solver_imp=.*/ark_solver_imp=ESDIRK4(3)6L[2]SA/' \
-#
-#for nb_sol_pt in ${SOL_PT_COUNTS}; do
-#    for nb_elem in ${ELEM_COUNTS}; do
-#        for dt in ${TIME_STEPS}; do
-#            sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
-#                                -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
-#                                -e 's/^dt=.*/dt='${dt}'/'
-#            ${RUN}
-#        done
-#    done
-#done
-#
-#sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp=ESDIRK4(3I)6L[2]SA/' \
-#                    -e 's/^ark_solver_imp=.*/ark_solver_imp=ESDIRK4(3I)6L[2]SA/' \
-#
-#for nb_sol_pt in ${SOL_PT_COUNTS}; do
-#    for nb_elem in ${ELEM_COUNTS}; do
-#        for dt in ${TIME_STEPS}; do
-#            sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
-#                                -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
-#                                -e 's/^dt=.*/dt='${dt}'/'
-#            ${RUN}
-#        done
-#    done
-#done
-
-sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp=QESDIRK4(3)6L[2]SA/' \
-                    -e 's/^ark_solver_imp=.*/ark_solver_imp=QESDIRK4(3)6L[2]SA/' \
-
-for nb_sol_pt in ${SOL_PT_COUNTS}; do
-    for nb_elem in ${ELEM_COUNTS}; do
-        for dt in ${TIME_STEPS}; do
-            sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
-                                -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
-                                -e 's/^dt=.*/dt='${dt}'/'
-            ${RUN}
+    for nb_sol_pt in ${SOL_PT_COUNTS}; do
+        for nb_elem in ${ELEM_COUNTS}; do
+            for dt in ${TIME_STEPS}; do
+                sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
+                                    -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
+                                    -e 's/^dt=.*/dt='${dt}'/'
+                ${RUN}
+            done
         done
     done
 done
-
-sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp=ESDIRK5(3)6L[2]SA/' \
-                    -e 's/^ark_solver_imp=.*/ark_solver_imp=ESDIRK5(3)6L[2]SA/' \
-
-for nb_sol_pt in ${SOL_PT_COUNTS}; do
-    for nb_elem in ${ELEM_COUNTS}; do
-        for dt in ${TIME_STEPS}; do
-            sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
-                                -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
-                                -e 's/^dt=.*/dt='${dt}'/'
-            ${RUN}
-        done
-    done
-done
-
-sed -i ${case_file} -e 's/^ark_solver_exp=.*/ark_solver_exp=ESDIRK5(4)7L[2]SA/' \
-                    -e 's/^ark_solver_imp=.*/ark_solver_imp=ESDIRK5(4)7L[2]SA/' \
-
-for nb_sol_pt in ${SOL_PT_COUNTS}; do
-    for nb_elem in ${ELEM_COUNTS}; do
-        for dt in ${TIME_STEPS}; do
-            sed -i ${case_file} -e 's/^nbsolpts=.*/nbsolpts='${nb_sol_pt}'/' \
-                                -e 's/^nb_elements=.*/nb_elements='${nb_elem}'/' \
-                                -e 's/^dt=.*/dt='${dt}'/'
-            ${RUN}
-        done
-    done
-done
-
 
