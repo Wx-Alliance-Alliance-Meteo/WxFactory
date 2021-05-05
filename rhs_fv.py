67114ed093d14d8ad875160d1a32cb914a634e56
Vincent Magnoux <vmagnoux20@gmail.com>
2021-05-05 14:54:25 +0000

    Saving rhs_fv for posterity

--- rhs_fv.py
+++ rhs_fv.py
@@ -3,53 +3,9 @@ import numpy
 from definitions  import idx_h, idx_hu1, idx_hu2, gravity
 from graphx       import plot_field, plot_array, plot_vector_field
 
-def rhs_sw_fv(Q, geom, mtrx, metric, topo, ptopo,
-              nbsolpts: int, nb_elements_horiz: int, case_number: int,
-              filter_rhs: bool = False):
-
-   datatype = Q.dtype
-   num_elements = Q.shape[1]
-   # print(f'Num_elements: {num_elements}')
-
-   h    = Q[idx_h, :, :]
-   h_u1 = Q[idx_hu1, :, :]
-   h_u2 = Q[idx_hu2, :, :]
-   u1   = h_u1 / h
-   u2   = h_u2 / h
-
-   h_squared = h**2
-   h_topo    = h + topo.hsurf
-
-   halo_offset = 1
-
-   flux_e0_x1 = h * metric.sqrtG * u1
-   flux_e0_x2 = h * metric.sqrtG * u2
-
-   flux_e1_x1 = metric.sqrtG * (h_u1 * u1 + 0.5 * gravity * metric.H_contra_11 * h_squared)
-   flux_e1_x2 = metric.sqrtG * (h_u1 * u2 + 0.5 * gravity * metric.H_contra_12 * h_squared)
-
-   flux_e2_x1 = metric.sqrtG * (h_u2 * u1 + 0.5 * gravity * metric.H_contra_21 * h_squared)
-   flux_e2_x2 = metric.sqrtG * (h_u2 * u2 + 0.5 * gravity * metric.H_contra_22 * h_squared)
-
-   # h_ext         = ptopo.xchange_halo(h)
-   # h_squared_ext = ptopo.xchange_halo(h_squared)
-   # sqrtG_ext     = ptopo.xchange_halo(metric.sqrtG)
-   #
-   # h11_ext = ptopo.xchange_halo(metric.H_contra_11)
-   # h12_ext = ptopo.xchange_halo(metric.H_contra_12)
-   # h21_ext = ptopo.xchange_halo(metric.H_contra_21)
-   # h22_ext = ptopo.xchange_halo(metric.H_contra_22)
-   #
-   # hu1_ext, hu2_ext = ptopo.xchange_halo_vector(geom, h_u1, h_u2)
-   u1_ext,  u2_ext  = ptopo.xchange_halo_vector(geom, u1, u2)
-   #
-   # f1_x1_ext = sqrtG_ext * (hu1_ext * u1_ext + 0.5 * gravity * h11_ext * h_squared_ext)
-   # f1_x2_ext = sqrtG_ext * (hu1_ext * u2_ext + 0.5 * gravity * h12_ext * h_squared_ext)
-
-   f1_x1_ext, f1_x2_ext = ptopo.xchange_halo_vector(geom, flux_e1_x1, flux_e1_x2)
-
-   # --- Compute flux at interfaces
-   # flux_itf = numpy.zeros((3, 2, 2, num_elements + 1, num_elements), dtype=datatype)
+def get_itf_flux(ptopo, geom, flux_e0_x1, flux_e0_x2, flux_e1_x1, flux_e1_x2, flux_e2_x1, flux_e2_x2):
+   datatype = flux_e0_x1.dtype
+   num_elements = flux_e0_x1.shape[0]
 
    f0_itf_ew = numpy.zeros((num_elements, num_elements + 1), dtype=datatype)
    f1_itf_ew = numpy.zeros_like(f0_itf_ew)
@@ -116,8 +72,8 @@ def rhs_sw_fv(Q, geom, mtrx, metric, topo, ptopo,
    f1_itf_ns[-1, :] += f1_n[1]
    f1_itf_ns[0, :]  += f1_s[1]
    f1_itf_ew[:, 0]  += f1_w[0]
-   # f1_itf_ew[:, 0]  = f1_w[0] * 2.0
-   # f1_itf_ew[:, 0]  *= 2.0
+   # f1_itf_ew[:, 0]  *= 1.5
+   # f1_itf_ew[:, 0]  += f1_w[0] * 0.5
    f1_itf_ew[:, -1] += f1_e[0]
 
    f2_n, f2_s, f2_w, f2_e = ptopo.xchange_simple_vectors(
@@ -140,6 +96,76 @@ def rhs_sw_fv(Q, geom, mtrx, metric, topo, ptopo,
    f1_itf_ns *= 0.5
    f2_itf_ns *= 0.5
 
+   return f0_itf_ew, f0_itf_ns, f1_itf_ew, f1_itf_ns, f2_itf_ew, f2_itf_ns
+
+
+def rhs_sw_fv(Q, geom, mtrx, metric, topo, ptopo,
+              nbsolpts: int, nb_elements_horiz: int, case_number: int,
+              filter_rhs: bool = False):
+
+   datatype = Q.dtype
+   num_elements = Q.shape[1]
+   # print(f'Num_elements: {num_elements}')
+
+   h    = Q[idx_h, :, :]
+   h_u1 = Q[idx_hu1, :, :]
+   h_u2 = Q[idx_hu2, :, :]
+   u1   = h_u1 / h
+   u2   = h_u2 / h
+
+   h_squared = h**2
+   h_topo    = h + topo.hsurf
+
+   halo_offset = 1
+
+   flux_e0_x1 = h * metric.sqrtG * u1
+   flux_e0_x2 = h * metric.sqrtG * u2
+
+   flux_e1_x1 = metric.sqrtG * (h_u1 * u1 + 0.5 * gravity * metric.H_contra_11 * h_squared)
+   flux_e1_x2 = metric.sqrtG * (h_u1 * u2 + 0.5 * gravity * metric.H_contra_12 * h_squared)
+
+   flux_e2_x1 = metric.sqrtG * (h_u2 * u1 + 0.5 * gravity * metric.H_contra_21 * h_squared)
+   flux_e2_x2 = metric.sqrtG * (h_u2 * u2 + 0.5 * gravity * metric.H_contra_22 * h_squared)
+
+   # h_ext         = ptopo.xchange_halo(h)
+   # h_squared_ext = ptopo.xchange_halo(h_squared)
+   # sqrtG_ext     = ptopo.xchange_halo(metric.sqrtG)
+   #
+   # h11_ext = ptopo.xchange_halo(metric.H_contra_11)
+   # h12_ext = ptopo.xchange_halo(metric.H_contra_12)
+   # h21_ext = ptopo.xchange_halo(metric.H_contra_21)
+   # h22_ext = ptopo.xchange_halo(metric.H_contra_22)
+   #
+   # hu1_ext, hu2_ext = ptopo.xchange_halo_vector(geom, h_u1, h_u2)
+   u1_ext,  u2_ext  = ptopo.xchange_halo_vector(geom, u1, u2)
+   #
+   # f1_x1_ext = sqrtG_ext * (hu1_ext * u1_ext + 0.5 * gravity * h11_ext * h_squared_ext)
+   # f1_x2_ext = sqrtG_ext * (hu1_ext * u2_ext + 0.5 * gravity * h12_ext * h_squared_ext)
+
+   f1_x1_ext, f1_x2_ext = ptopo.xchange_halo_vector(geom, flux_e1_x1, flux_e1_x2)
+
+   # --- Compute flux at interfaces
+   # flux_itf = numpy.zeros((3, 2, 2, num_elements + 1, num_elements), dtype=datatype)
+
+   f0_itf_ew, f0_itf_ns, f1_itf_ew, f1_itf_ns, f2_itf_ew, f2_itf_ns = get_itf_flux(
+      ptopo, geom, flux_e0_x1, flux_e0_x2, flux_e1_x1, flux_e1_x2, flux_e2_x1, flux_e2_x2)
+
+
+   lon_w_local = geom.lon_itf_i[:, 0]
+   lon_e_local = geom.lon_itf_i[:, -1]
+   lon_s_local = geom.lon_itf_j[0, :]
+   lon_n_local = geom.lon_itf_j[-1, :]
+
+   lat_w_local = geom.lat_itf_i[:, 0]
+   lat_e_local = geom.lat_itf_i[:, -1]
+   lat_s_local = geom.lat_itf_j[0, :]
+   lat_n_local = geom.lat_itf_j[-1, :]
+
+   lon_n, lon_s, lon_w, lon_e = ptopo.send_recv_neighbors(lon_n_local, lon_s_local, lon_w_local, lon_e_local, 0)
+   lat_n, lat_s, lat_w, lat_e = ptopo.send_recv_neighbors(lat_n_local, lat_s_local, lat_w_local, lat_e_local, 0)
+
+   print(f'Lat_w: local \n{lat_w_local}\n    neighbor\n{lat_w}')
+   print(f'Lon_w: local \n{lon_w_local}\n    neighbor\n{lon_w}')
 
    #TODO Should divide these by elem size ?
    df0_dx1 = f0_itf_ew[:, 1:] - f0_itf_ew[:, :-1]
@@ -149,6 +175,11 @@ def rhs_sw_fv(Q, geom, mtrx, metric, topo, ptopo,
    df0_dx2 = f0_itf_ns[1:, :] - f0_itf_ns[:-1, :]
    df1_dx2 = f1_itf_ns[1:, :] - f1_itf_ns[:-1, :]
    df2_dx2 = f2_itf_ns[1:, :] - f2_itf_ns[:-1, :]
+
+   # df1_dx1[:,  0] = (f1_itf_ew[:, 1]   - flux_e1_x1[:, 0]) * 2.0
+   # df1_dx1[:, -1] = (flux_e1_x1[:, -1] - f1_itf_ew[:, -2]) * 2.0
+   # df1_dx2[0,  :] = (f1_itf_ns[1, :]   - flux_e1_x2[0, :]) * 2.0
+   # df1_dx2[-1, :] = (flux_e1_x2[-1, :] - f1_itf_ns[-2, :]) * 2.0
 
    # use_west  = f1_itf_ew[:, 1:] > f1_itf_ew[:, :-1]
    # df0_dx1 = f0_itf_ew[:, 1:]
@@ -171,11 +202,11 @@ def rhs_sw_fv(Q, geom, mtrx, metric, topo, ptopo,
    # plot_array(flux_e0_x1, filename='flux_e0_x1.png')
    # plot_array(f0_itf_ew[:, :-1], filename='f_itf_ew.png')
 
-   plot_array(f0_itf_ew[:, :-1], filename='f0_itf_w_fv.png')
-   plot_array(f1_itf_ew[:, :-1], filename='f1_itf_w_fv.png')
-   plot_array(f1_itf_ew[:, 1:], filename='f1_itf_e_fv.png')
-   plot_array(f1_itf_ns[1:, :], filename='f1_itf_n_fv.png')
-   plot_array(f1_itf_ns[:-1, :], filename='f1_itf_s_fv.png')
+   plot_array(f0_itf_ew[:, :], filename='f0_itf_ew_fv.png')
+   plot_array(f1_itf_ew[:, :], filename='f1_itf_ew_fv.png')
+   # plot_array(f1_itf_ew[:, 1:], filename='f1_itf_e_fv.png')
+   plot_array(f1_itf_ns[:, :], filename='f1_itf_ns_fv.png')
+   # plot_array(f1_itf_ns[:-1, :], filename='f1_itf_s_fv.png')
    plot_array(f2_itf_ew[:, :-1], filename='f2_itf_w_fv.png')
 
    plot_array(flux_e1_x1, filename='f1_x1_fv.png')
@@ -186,6 +217,10 @@ def rhs_sw_fv(Q, geom, mtrx, metric, topo, ptopo,
 
    plot_array(f1_x1_ext, filename='f1_x1_full.png')
    plot_array(f1_x2_ext, filename='f1_x2_full.png')
+
+   df1_dx1_ext, df1_dx2_ext = ptopo.xchange_halo_vector(geom, df1_dx1, df1_dx2)
+   plot_array(df1_dx1_ext, filename='df1_dx1_full_fv.png')
+   plot_array(df1_dx2_ext, filename='df1_dx2_full_fv.png')
 
    # plot_array(f0_itf_ns[:-1, :], filename='f_itf_ns.png')
    # plot_array(df0_dx1, filename='f0_itf.png')
