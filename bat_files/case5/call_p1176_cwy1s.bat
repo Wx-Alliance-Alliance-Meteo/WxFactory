61f7f847e54f8c2f26dca4f624ac968549e932b2
Tanya Tafolla <ttafolla@rclogin02.cluster>
2023-04-03 10:14:20 -0700

    added .bat files for processor 1176, 2646, 4704

--- bat_files/case5/call_p1176_cwy1s.bat
+++ bat_files/case5/call_p1176_cwy1s.bat
@@ -3,14 +3,11 @@
 #SBATCH -J gef_p1176
 #SBATCH -o gef_p1176.out
 #SBATCH -e gef_p1176.error
-#SBATCH -N 8  #how many cluster nodes #STEPHANE HAS TO CHANGE
+#SBATCH -N 15  #how many cluster nodes #STEPHANE HAS TO CHANGE
 #SBATCH -n 1176  #how many cores
 #SBATCH --exclusive
-#SBATCH --constraint=ib
 #SBATCH -t 03:00:00
 #SBATCH --array=1-7
-
-conda activate gef3
 
 #srun --mpi=pmi2 --cpu-bind=cores python3 ./main_gef.py config/case6.ini
 
---
+++ bat_files/case5/call_p4704_pmex1s.bat
@@ -0,0 +1,14 @@
+#! /bin/bash -l
+
+#SBATCH -J gef_p4704
+#SBATCH -o gef_p4704.out
+#SBATCH -e gef_p4704.error
+#SBATCH -N 59  #how many cluster nodes #STEPHANE HAS TO CHANGE
+#SBATCH -n 4704  #how many cores
+#SBATCH --exclusive
+#SBATCH -t 03:00:00
+#SBATCH --array=1-7
+
+#srun --mpi=pmi2 --cpu-bind=cores python3 ./main_gef.py config/case6.ini
+
+mpirun -np 4704 python3 ./main_gef.py config/procs4704/case5_pmex1s.ini
