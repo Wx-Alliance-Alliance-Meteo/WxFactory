c89377cde0fe32ef4317c6d45d83b2ae3b5d67bb
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-04-17 08:48:12 -0700

    added more steps for simulation, 16 to 48 timesteps

--- run_all.sh
+++ run_all.sh
@@ -1,27 +1,30 @@
 
-methods=("call_p294_og.bat" "call_p294_cwy1s.bat" "call_p294_cwyne.bat" "call_p294_cwyne1s.bat" "call_p294_icwy1s.bat" "call_p294_icwyne.bat" "call_p294_icwy_ne1s.bat" "call_p294_icwyiop.bat" "call_p294_pmex1s.bat" "call_p294_pmexne.bat" "call_p294_pmexne1s.bat" "call_p294_kiopsne.bat")
-methodlen=${#methods[@]}
 
 #for case 5
-for ((k = 0; k < $methodlen; k++)); do
-    ord_soumet -cpus 294                                    \
-               -w 180                                       \
-               -jn gef_294_c5_${k}                          \
-               -mpi                                         \
-               -clone 7                                     \
-               -share e                                     \
-               -jobfile bat_files/case5/${methods[$k]}      \
-               -listing $(pwd)/listings
-done
+ord_soumet -cpus 294                                    \
+           -w 180                                       \
+           -jn gef_294_c5                               \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case5/call_p294.bat       \
+           -listing $(pwd)/listings
 
 #for case 6
-for ((k = 0; k < $methodlen; k++)); do
-    ord_soumet -cpus 294 -jn gef_294_c6_${k} -mpi -clone 7 -share e -jobfile bat_files/case6/${methods[$k]} -listing $(pwd)/listings
-done
+ord_soumet -cpus 294                                    \
+           -w 180                                       \
+           -jn gef_294_c6                               \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case6/call_p294.bat       \
+           -listing $(pwd)/listings
 
 #for case 8
-for ((k = 0; k < $methodlen; k++)); do
-    ord_soumet -cpus 294 -jn gef_294_c8_${k} -mpi -clone 7 -share e -jobfile bat_files/case8/${methods[$k]} -listing $(pwd)/listings
-done
+ord_soumet -cpus 294                                    \
+           -w 180                                       \
+           -jn gef_294_c8                               \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/call_p294.bat       \
+           -listing $(pwd)/listings
 
 
