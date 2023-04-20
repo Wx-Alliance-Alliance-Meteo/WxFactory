7c58ae8543d1dfbbe036f37af8e3d78a724a0ce1
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-04-20 13:19:42 -0700

    updated run_all for next test

--- run_all.sh
+++ run_all.sh
@@ -1,30 +1,30 @@
 
 
 #for case 5
-ord_soumet -cpus 1176                                    \
+ord_soumet -cpus 2646                                    \
            -w 180                                       \
-           -jn gef_1176_c5                               \
+           -jn gef_2646_c5                               \
            -mpi                                         \
            -share e                                     \
-           -jobfile bat_files/case5/call_p1176.bat       \
+           -jobfile bat_files/case5/call_p2646.bat       \
            -listing $(pwd)/listings
 
 #for case 6
-ord_soumet -cpus 1176                                    \
+ord_soumet -cpus 2646                                    \
            -w 180                                       \
-           -jn gef_1176_c6                               \
+           -jn gef_2646_c6                               \
            -mpi                                         \
            -share e                                     \
-           -jobfile bat_files/case6/call_p1176.bat       \
+           -jobfile bat_files/case6/call_p2646.bat       \
            -listing $(pwd)/listings
 
 #for case 8
-ord_soumet -cpus 1176                                    \
+ord_soumet -cpus 2646                                    \
            -w 180                                       \
-           -jn gef_1176_c8                               \
+           -jn gef_2646_c8                               \
            -mpi                                         \
            -share e                                     \
-           -jobfile bat_files/case8/call_p1176.bat       \
+           -jobfile bat_files/case8/call_p2646.bat       \
            -listing $(pwd)/listings
 
 
