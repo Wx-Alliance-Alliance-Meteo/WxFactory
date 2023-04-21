8f4a2fd8c3c28c8a733a1c0b0e902f568199933e
Tanya Tafolla <ttafolla@rclogin02.cluster>
2023-04-21 09:59:27 -0700

    set up next test

--- run_all.sh
+++ run_all.sh
@@ -1,30 +1,30 @@
 
 
 #for case 5
-ord_soumet -cpus 2646                                    \
+ord_soumet -cpus 4704                                    \
            -w 180                                       \
-           -jn gef_2646_c5                               \
+           -jn gef_4704_c5                               \
            -mpi                                         \
            -share e                                     \
-           -jobfile bat_files/case5/call_p2646.bat       \
+           -jobfile bat_files/case5/call_p4704.bat       \
            -listing $(pwd)/listings
 
 #for case 6
-ord_soumet -cpus 2646                                    \
+ord_soumet -cpus 4704                                    \
            -w 180                                       \
-           -jn gef_2646_c6                               \
+           -jn gef_4704_c6                               \
            -mpi                                         \
            -share e                                     \
-           -jobfile bat_files/case6/call_p2646.bat       \
+           -jobfile bat_files/case6/call_p4704.bat       \
            -listing $(pwd)/listings
 
 #for case 8
-ord_soumet -cpus 2646                                    \
+ord_soumet -cpus 4704                                    \
            -w 180                                       \
-           -jn gef_2646_c8                               \
+           -jn gef_4704_c8                               \
            -mpi                                         \
            -share e                                     \
-           -jobfile bat_files/case8/call_p2646.bat       \
+           -jobfile bat_files/case8/call_p4704.bat       \
            -listing $(pwd)/listings
 
 
