312e81c1433ddb28a1755400bdfc80dbc7902d6f
Tanya Tafolla <ttafolla@rclogin02.cluster>
2023-04-24 13:31:58 -0700

    run tests that did not finish in last run

--- bat_files/case8/call_p864.bat
+++ bat_files/case8/call_p864.bat
@@ -2,7 +2,9 @@
 cd /home/vma000/ords/gef_tanya
 . ./load_env.sh
 
-methods=("galewsky.ini" "galewsky_cwy1s.ini" "galewsky_cwyne.ini" "galewsky_cwyne1s.ini" "galewsky_icwy1s.ini" "galewsky_icwyne.ini" "galewsky_icwyne1s.ini" "galewsky_icwyiop.ini" "galewsky_pmex1s.ini" "galewsky_pmexne.ini" "galewsky_pmexne1s.ini")
+#methods=("galewsky.ini" "galewsky_cwy1s.ini" "galewsky_cwyne.ini" "galewsky_cwyne1s.ini" "galewsky_icwy1s.ini" "galewsky_icwyne.ini" "galewsky_icwyne1s.ini" "galewsky_icwyiop.ini" "galewsky_pmex1s.ini" "galewsky_pmexne.ini" "galewsky_pmexne1s.ini")
+
+methods=("galewsky_kiopsne.ini" "galewsky_cwyne.ini")
 methodlen=${#methods[@]}
 
 #looping through each case and running it 7 times
--- run_all.sh
+++ run_all.sh
@@ -1,30 +1,128 @@
 
+#re-running some test because they didn't run the firs time.
+#maybe they ran out of time?
 
 #for case 5
-ord_soumet -cpus 4704                                    \
+ord_soumet -cpus 294                                    \
            -w 180                                       \
-           -jn gef_4704_c5                               \
+           -jn gef_294_c5                               \
            -mpi                                         \
            -share e                                     \
-           -jobfile bat_files/case5/call_p4704.bat       \
+           -jobfile bat_files/case5/call_p294.bat       \
            -listing $(pwd)/listings
 
 #for case 6
-ord_soumet -cpus 4704                                    \
+ord_soumet -cpus 294                                    \
            -w 180                                       \
-           -jn gef_4704_c6                               \
+           -jn gef_294_c6                               \
            -mpi                                         \
            -share e                                     \
-           -jobfile bat_files/case6/call_p4704.bat       \
+           -jobfile bat_files/case6/call_p294.bat       \
            -listing $(pwd)/listings
 
 #for case 8
-ord_soumet -cpus 4704                                    \
+ord_soumet -cpus 294                                    \
            -w 180                                       \
-           -jn gef_4704_c8                               \
+           -jn gef_294_c8                               \
            -mpi                                         \
            -share e                                     \
-           -jobfile bat_files/case8/call_p4704.bat       \
+           -jobfile bat_files/case8/call_p294.bat       \
            -listing $(pwd)/listings
+
+#-------------------------------------------------------
+
+#for case 5
+ord_soumet -cpus 864                                    \
+           -w 180                                       \
+           -jn gef_864_c5                               \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case5/call_p864.bat       \
+           -listing $(pwd)/listings
+
+#for case 6
+ord_soumet -cpus 864                                    \
+           -w 180                                       \
+           -jn gef_864_c6                               \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case6/call_p864.bat       \
+           -listing $(pwd)/listings
+
+#for case 8
+ord_soumet -cpus 864                                    \
+           -w 180                                       \
+           -jn gef_864_c8                               \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/call_p864.bat       \
+           -listing $(pwd)/listings
+
+#---------------------------------------------------------
+
+#for case 6
+ord_soumet -cpus 1176                                  \
+           -w 180                                      \
+           -jn gef_1176_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/call_p1176.bat     \
+           -listing $(pwd)/listings
+
+#for case 6
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
+#for case 6
+ord_soumet -cpus 4704                                  \
+           -w 180                                      \
+           -jn gef_4704_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/call_p4704.bat     \
+           -listing $(pwd)/listings
+
+#----------------------------------------------------------
+
+#for case 8
+ord_soumet -cpus 1176                                   \
+           -w 180                                       \
+           -jn gef_1176_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/call_p1176.bat      \
+           -listing $(pwd)/listings
+
+#for case 8
+ord_soumet -cpus 2646                                   \
+           -w 180                                       \
+           -jn gef_2646_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/call_p2646.bat      \
+           -listing $(pwd)/listings
+
+
+#for case 8
+ord_soumet -cpus 4704                                   \
+           -w 180                                       \
+           -jn gef_4704_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/call_p4704.bat      \
+           -listing $(pwd)/listings
+
+
+
+
+
+
+
 
 
