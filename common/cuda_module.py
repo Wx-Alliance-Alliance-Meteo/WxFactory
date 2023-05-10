76dd52f0b40e58236caa49208b632126a68a9fb5
Brennan Lockinger <brl423@mail.usask.ca>
2023-05-10 11:06:24 -0600

    finished and improved cuda interface

--- common/cuda_module.py
+++ common/cuda_module.py
@@ -4,51 +4,58 @@ Provide a smooth interface between .cu files and python code.
 
 import os
 import inspect
+import warnings
+from dataclasses import dataclass, field
 import cupy as cp
 
 from os import PathLike
-from typing import Callable, Iterable, TypeVarTuple, Generic, Self
+from typing import Callable, Iterable, Iterator, TypeVarTuple, Generic, Self
+
+CUDA_DEVICE_COUNT: int = cp.cuda.runtime.getDeviceCount()
+CUDA_THREAD_LIMIT      = tuple[int, ...](cp.cuda.runtime.getDeviceProperties(i)["maxThreadsPerBlock"] for i in range(CUDA_DEVICE_COUNT))
+CUDA_BLOCK_SHAPE       = tuple[tuple[int, int, int], ...](cp.cuda.runtime.getDeviceProperties(i)["maxThreadsDim"] for i in range(CUDA_DEVICE_COUNT))
+CUDA_GRID_SHAPE        = tuple[tuple[int, int, int], ...](cp.cuda.runtime.getDeviceProperties(i)["maxGridSize"] for i in range(CUDA_DEVICE_COUNT))
+
+
+@dataclass(slots=True)
+class Dim:
+
+    x: int = field(default=1)
+    y: int = field(default=1)
+    z: int = field(default=1)
+
+    @property
+    def tuple(self) -> tuple[int, int, int]:
+        return (self.x, self.y, self.z)
+    
+    def __iter__(self) -> Iterator[int]:
+        def iterator():
+            yield self.x
+            yield self.y
+            yield self.z
+        return iterator()
+    
+    def __getitem__(self, idx: int) -> int:
+        return getattr(self, ("x", "y", "z")[idx])
+    
+    def __setitem__(self, idx: int, value: int):
+        setattr(self, ("x", "y", "z")[idx], value)
 
 KernelArgs = TypeVarTuple("KernelArgs")
-DimSpec    = int | tuple[int] | tuple[int, int] | tuple[int, int, int]
-DimSpecs   = tuple[DimSpec, DimSpec]
-DimFun     = Callable[[*KernelArgs], DimSpecs]
-RawKernel  = Callable[[DimSpec, DimSpec, *KernelArgs], None]
+DimFun     = Callable[[*KernelArgs], tuple[Dim, Dim]]
+RawKernel  = Callable[[Dim, Dim, *KernelArgs], None]
 
-CUDA_DEVICE_COUNT = cp.cuda.runtime.getDeviceCount()
-CUDA_THREAD_LIMIT = tuple(cp.cuda.runtime.getDeviceProperties(i)["maxThreadsPerBlock"] for i in range(CUDA_DEVICE_COUNT))
-CUDA_BLOCK_SHAPE  = tuple(cp.cuda.runtime.getDeviceProperties(i)["maxThreadsDim"] for i in range(CUDA_DEVICE_COUNT))
-CUDA_GRID_SHAPE   = tuple(cp.cuda.runtime.getDeviceProperties(i)["maxGridSize"] for i in range(CUDA_DEVICE_COUNT))
-
-class CudaKernel(Generic[*KernelArgs]):
-
-    def __init__(self: Self,
-                 dimspec: DimSpecs | DimFun[*KernelArgs],
-                 f: RawKernel | None = None):
-        if callable(dimspec):
-            self.dims = dimspec
-        else:
-            self.dims = lambda *_: dimspec
-        self.__wrapped__ = f
-
-    def __call__(self: Self, *args: *KernelArgs):
-        if self.__wrapped__:
-            gridspec, blockspec = self.dims(*args)
-            self.__wrapped__(gridspec, blockspec, args)
-        else:
-            raise AttributeError
-
-def cuda_kernel(dimspec: DimSpecs | DimFun[*KernelArgs]) -> Callable[[RawKernel[*KernelArgs]], CudaKernel]:
-    return lambda _: CudaKernel(dimspec, None)
 
 class CudaModule(object):
 
-    def __init__(self: Self,
-                 path: PathLike,
-                 name_expressions: Iterable[str] = (),
-                 cpp_standard: str | None = None,
-                 path_lexically_relative: bool = True):
-        super().__init__()
+    @classmethod
+    def __init_subclass__(cls, /,
+                          path: PathLike,
+                          name_expressions: Iterable[str] | None = None,
+                          cpp_standard: str | None = None,
+                          path_lexically_relative: bool = True,
+                          **kwargs):
+        super().__init_subclass__(**kwargs)
 
         if path_lexically_relative and not os.path.isabs(path):
             caller = inspect.stack()[0]
@@ -60,12 +67,67 @@ class CudaModule(object):
         
         options = (f"-std={cpp_standard}",) if cpp_standard else ()
 
-        self.module = cp.RawModule(code=code, name_expressions=name_expressions, options=options)
+        cls.module = cp.RawModule(code=code, name_expressions=name_expressions, options=options)
 
-    def __getattr__(self: Self, name: str) -> Callable[..., None]:
-        try:
-            f = self.module.get_function(name)
-            setattr(self, name, f)
-            return f
-        except:
-            raise
+    @classmethod
+    def get_function(cls: type[Self], name: str) -> RawKernel:
+        return cls.module.get_function(name)
+
+
+class CudaKernel(Generic[*KernelArgs]):
+
+    def __init__(self: Self,
+                 dimspec: DimFun[*KernelArgs],
+                 f: RawKernel | None = None,
+                 name: str | None = None):
+        if callable(dimspec):
+            self.dims = dimspec
+        else:
+            self.dims = lambda *_: dimspec
+        self.__wrapped__ = f
+        self.name = name
+
+    def __set_name__(self, owner: CudaModule, name: str):
+        self.name = name
+
+    def __call__(self: Self, *args: *KernelArgs) -> None:
+        if self.__wrapped__:
+            gridspec, blockspec = self.dims(*args)
+            self.__wrapped__(gridspec.tuple, blockspec.tuple, args)
+        else:
+            warnings.warn(f"")
+        
+    def __get__(self: Self, instance: CudaModule | None, owner: type[CudaModule] | None = None) -> Self:
+        if not self.__wrapped__ and self.name:
+            obj = instance if instance else owner
+            self.__wrapped__ = obj.get_function(self.name)
+        return self
+
+    
+class DimSpec:
+
+    x, y, z = 0, 1, 2
+    
+    @classmethod
+    def groupby(cls: type[Self], dim: int, arg: int = 0, shape: Callable[[tuple[int, ...]], Dim] = lambda x: Dim(*x)) -> DimFun[*KernelArgs]:
+        def ret(*args: *KernelArgs) -> tuple[Dim, Dim]:
+            s = shape(args[arg].shape)
+            dev: int = cp.cuda.runtime.getDevice()
+            thread_lim = min(CUDA_THREAD_LIMIT[dev], CUDA_BLOCK_SHAPE[dev][dim])
+            gridspec, blockspec = list(s), [1] * 3
+            blocks = (lambda d: d[0] + (1 if d[1] else 0))(divmod(s[dim], thread_lim))
+            gridspec[dim] = blocks
+            gridspec = Dim(*gridspec)
+            if blocks > CUDA_GRID_SHAPE[dev][dim]:
+                raise NotImplementedError(f"Grid {gridspec} too large for CUDA device {dev}")
+            blockspec[dim] = thread_lim
+            return gridspec, Dim(*blockspec)
+        return ret
+    
+    @classmethod
+    def groupby_first_x(cls: type[Self], shape: Callable[[tuple[int, ...]], Dim] = lambda x: Dim(*x)) -> DimFun[*KernelArgs]:
+        return cls.groupby(cls.x, 0, shape)
+
+
+def cuda_kernel(dimspec: DimFun[*KernelArgs]) -> Callable[[RawKernel[*KernelArgs]], CudaKernel[*KernelArgs]]:
+    return lambda _: CudaKernel(dimspec, None)
