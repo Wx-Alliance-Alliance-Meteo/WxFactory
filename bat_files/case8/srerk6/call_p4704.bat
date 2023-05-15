962d189232649422220e9189e5b48da6cdbec37a
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-05-15 14:39:05 -0700

    srerk files for case8

---
+++ bat_files/case8/srerk6/call_p4704.bat
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+cd /home/vma000/ords/gef_tanya
+. ./load_env.sh
+
+methods=("galewsky_kiops.ini" "galewsky_cwy1s.ini" "galewsky_cwyne.ini" "galewsky_cwyne1s.ini" "galewsky_icwy1s.ini" "galewsky_icwyne.ini" "galewsky_icwyne1s.ini" "galewsky_icwyiop.ini" "galewsky_pmex1s.ini" "galewsky_pmexne.ini" "galewsky_pmexne1s.ini" "galewsky_kiops_ne.ini")
+methodlen=${#methods[@]}
+
+#looping through each case and running it 7 times
+for ((k=0; k < $methodlen; k++)); do
+   for ((j = 0; j < 7; j++)); do
+     mpirun -np 4704 python3 ./main_gef.py config/test_files/srerk6/${methods[${k}]}
+  done
+done
