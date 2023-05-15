b8d49f44c0529c4414b20f10e7559652bef473b4
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-05-15 15:50:16 -0700

    broke up large/small processors

---
+++ run_med.sh
@@ -0,0 +1,327 @@
+
+
+#----------------------2646---------------------------------
+#for case 5
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c5                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case5/epi2/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
+
+#for case 6
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/epi2/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
+#for case 8
+ord_soumet -cpus 2646                                   \
+           -w 180                                       \
+           -jn gef_2646_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/epi2/call_p2646.bat      \
+           -listing $(pwd)/listings
+
+
+#for case 5
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c5                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case5/epi4/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
+
+#for case 6
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/epi4/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
+#for case 8
+ord_soumet -cpus 2646                                   \
+           -w 180                                       \
+           -jn gef_2646_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/epi4/call_p2646.bat      \
+           -listing $(pwd)/listings
+
+
+#for case 5
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c5                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case5/epi6/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
+
+#for case 6
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/epi6/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
+#for case 8
+ord_soumet -cpus 2646                                   \
+           -w 180                                       \
+           -jn gef_2646_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/epi6/call_p2646.bat      \
+           -listing $(pwd)/listings
+
+
+#for case 5
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c5                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case5/srerk3/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
+
+#for case 6
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/srerk3/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
+#for case 8
+ord_soumet -cpus 2646                                   \
+           -w 180                                       \
+           -jn gef_2646_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/srerk3/call_p2646.bat      \
+           -listing $(pwd)/listings
+
+
+#for case 5
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c5                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case5/srerk6/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
+
+#for case 6
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/srerk6/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
+#for case 8
+ord_soumet -cpus 2646                                   \
+           -w 180                                       \
+           -jn gef_2646_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/srerk6/call_p2646.bat      \
+
+
+#----------------------1176---------------------------------
+#for case 5
+ord_soumet -cpus 1176                                  \
+           -w 180                                      \
+           -jn gef_1176_c5                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case5/epi2/call_p1176.bat     \
+           -listing $(pwd)/listings
+
+
+
+#for case 6
+ord_soumet -cpus 1176                                  \
+           -w 180                                      \
+           -jn gef_1176_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/epi2/call_p1176.bat     \
+           -listing $(pwd)/listings
+
+
+#for case 8
+ord_soumet -cpus 1176                                   \
+           -w 180                                       \
+           -jn gef_1176_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/epi2/call_p1176.bat      \
+           -listing $(pwd)/listings
+
+
+#for case 5
+ord_soumet -cpus 1176                                  \
+           -w 180                                      \
+           -jn gef_1176_c5                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case5/epi4/call_p1176.bat     \
+           -listing $(pwd)/listings
+
+
+
+#for case 6
+ord_soumet -cpus 1176                                  \
+           -w 180                                      \
+           -jn gef_1176_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/epi4/call_p1176.bat     \
+           -listing $(pwd)/listings
+
+
+#for case 8
+ord_soumet -cpus 1176                                   \
+           -w 180                                       \
+           -jn gef_1176_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/epi4/call_p1176.bat      \
+           -listing $(pwd)/listings
+
+
+#for case 5
+ord_soumet -cpus 1176                                  \
+           -w 180                                      \
+           -jn gef_1176_c5                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case5/epi6/call_p1176.bat     \
+           -listing $(pwd)/listings
+
+
+
+#for case 6
+ord_soumet -cpus 1176                                  \
+           -w 180                                      \
+           -jn gef_1176_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/epi6/call_p1176.bat     \
+           -listing $(pwd)/listings
+
+
+#for case 8
+ord_soumet -cpus 1176                                   \
+           -w 180                                       \
+           -jn gef_1176_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/epi6/call_p1176.bat      \
+           -listing $(pwd)/listings
+
+
+#for case 5
+ord_soumet -cpus 1176                                  \
+           -w 180                                      \
+           -jn gef_1176_c5                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case5/srerk3/call_p1176.bat     \
+           -listing $(pwd)/listings
+
+
+
+#for case 6
+ord_soumet -cpus 1176                                  \
+           -w 180                                      \
+           -jn gef_1176_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/srerk3/call_p1176.bat     \
+           -listing $(pwd)/listings
+
+
+#for case 8
+ord_soumet -cpus 1176                                   \
+           -w 180                                       \
+           -jn gef_1176_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/srerk3/call_p1176.bat      \
+           -listing $(pwd)/listings
+
+
+#for case 5
+ord_soumet -cpus 1176                                  \
+           -w 180                                      \
+           -jn gef_1176_c5                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case5/srerk6/call_p1176.bat     \
+           -listing $(pwd)/listings
+
+
+
+#for case 6
+ord_soumet -cpus 1176                                  \
+           -w 180                                      \
+           -jn gef_1176_c6                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case6/srerk6/call_p1176.bat     \
+           -listing $(pwd)/listings
+
+
+#for case 8
+ord_soumet -cpus 1176                                   \
+           -w 180                                       \
+           -jn gef_1176_c8                              \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/case8/srerk6/call_p1176.bat      \
+           -listing $(pwd)/listings
+
+
+
+
+           -listing $(pwd)/listings
+
+
+
+
+
+
+
+
+
+
+
