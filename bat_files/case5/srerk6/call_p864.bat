70a60a159516f49417bee6a984cbe94d3344631c
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-05-15 14:25:12 -0700

    srerk bat files

---
+++ bat_files/case5/srerk6/call_p864.bat
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+cd /home/vma000/ords/gef_tanya
+. ./load_env.sh
+
+methods=("case5_kiops.ini" "case5_cwy1s.ini" "case5_cwyne.ini" "case5_cwyne1s.ini" "case5_icwy1s.ini" "case5_icwyne.ini")
+
+#methods=("case5_kiopsne.ini")
+methodlen=${#methods[@]}
+
+#looping through each case and running it 7 times
+
+for ((k=0; k < $methodlen; k++)); do
+   for ((j = 0; j < 7; j++)); do
+     mpirun -np 864 python3 ./main_gef.py config/test_files/srerk6/${methods[${k}]}
+   done
+done
