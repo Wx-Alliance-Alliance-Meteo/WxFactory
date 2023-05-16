957d6c0da31c9a3a9b0b1a952872886acd31e5a8
Tanya Tafolla <ttafolla@rclogin01.cluster>
2023-05-16 08:00:56 -0700

    splitting 2k jobs

---
+++ run_med_srerk3.sh
@@ -0,0 +1,22 @@
+
+
+#----------------------2646---------------------------------
+#for case 5 and case 6
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_epi2                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/call_p2646_srerk3.bat     \
+           -listing $(pwd)/listings
+
+#for case 8
+ord_soumet -cpus 2646                                  \
+           -w 180                                      \
+           -jn gef_2646_c8                             \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/case8/srerk3/call_p2646.bat     \
+           -listing $(pwd)/listings
+
+
