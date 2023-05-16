a298831ae3459a47daa169ef77fe46de070ac4e1
Tanya Tafolla <ttafolla@rclogin01.cluster>
2023-05-16 07:39:35 -0700

    joined all cases for calls for 4k/10k, and split up the calls to the cluster by method

---
+++ run_large_epi6.sh
@@ -0,0 +1,25 @@
+
+#----------------------4704---------------------------------
+
+#for case 8
+ord_soumet -cpus 4704                                   \
+           -w 180                                       \
+           -jn gef_4704_epi6                             \
+           -mpi                                         \
+           -share e                                     \
+           -jobfile bat_files/call_p4704_epi6.bat      \
+           -listing $(pwd)/listings
+
+
+#----------------------10584---------------------------------
+#for case 5
+ord_soumet -cpus 10584                                  \
+           -w 180                                      \
+           -jn gef_10584_epi6                            \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/call_p10584_epi6.bat     \
+           -listing $(pwd)/listings
+
+
+
