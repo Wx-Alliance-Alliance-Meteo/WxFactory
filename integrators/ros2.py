dd2b78d73a9a2f4fab18f50dd1641e02d77632ac
Stéphane Gaudreault <stephane.gaudreault2@canada.ca>
2023-06-13 15:59:46 -0400

    Implement ML first guess in ROS2

--- integrators/ros2.py
+++ integrators/ros2.py
@@ -1,4 +1,5 @@
 import numpy
+import scipy
 from time import time
 from typing import Callable
 
@@ -14,11 +15,17 @@ class Ros2(Integrator):
       self.rhs_handle     = rhs_handle
       self.tol            = param.tolerance
       self.gmres_restart  = param.gmres_restart
+      self.X = None
+      self.B = None
+      self.nstep = 0
+      self.bsize = 8 # TODO : config
+      self.pointer = 0
 
    def __step__(self, Q: numpy.ndarray, dt: float):
 
       rhs    = self.rhs_handle(Q)
       Q_flat = Q.flatten()
+      n      = Q_flat.shape[0]
 
       def A(v):
          return matvec_rat(v, dt, Q, rhs, self.rhs_handle)
@@ -27,13 +34,28 @@ class Ros2(Integrator):
 
       maxiter = 20000 // self.gmres_restart
       if self.preconditioner is not None:
-         # maxiter = 200 // self.gmres_restart
-         maxiter = 420 // self.gmres_restart
-         maxiter = min(2, maxiter)
+         maxiter = 200 // self.gmres_restart
+
+      print('Initial ', numpy.linalg.norm(b - A(Q_flat)))
+
+      if self.nstep+1 >= 4 and self.X is not None:
+         BB = self.B.T
+         XX = self.X.T
+
+         pinvBB = numpy.linalg.pinv(BB)
+         approxInvA = scipy.sparse.linalg.LinearOperator((n,n), matvec=lambda vv: XX @ (pinvBB @ vv))
+
+         Q_x0 = approxInvA(b)
+         extrap = numpy.linalg.norm(b - A(Q_x0))
+         print('Extrap  ', extrap)
+
+      else:
+         Q_x0 = Q_flat.copy()
+
 
       t0 = time()
       Qnew, norm_r, norm_b, num_iter, flag, residuals = fgmres(
-         A, b, x0=Q_flat, tol=self.tol, restart=self.gmres_restart, maxiter=maxiter, preconditioner=self.preconditioner,
+         A, b, x0=Q_x0, tol=self.tol, restart=self.gmres_restart, maxiter=maxiter, preconditioner=self.preconditioner,
          verbose=self.verbose_solver)
       t1 = time()
 
@@ -41,9 +63,18 @@ class Ros2(Integrator):
 
       if MPI.COMM_WORLD.rank == 0:
          result_type = 'convergence' if flag == 0 else 'stagnation/interruption'
-         print(f'FGMRES {result_type} at iteration {num_iter} in {t1 - t0:4.1f} s to a solution with'
+         print(f'FGMRES {result_type} at iteration {num_iter} in {t1 - t0:4.3f} s to a solution with'
                f' relative residual {norm_r/norm_b : .2e}')
 
-      self.failure_flag = flag
+
+      if self.X is None:
+         self.B = numpy.zeros((self.bsize, n))
+         self.X = numpy.zeros((self.bsize, n))
+
+      self.X[self.pointer, :] = Qnew
+      self.B[self.pointer, :] = b
+
+      self.nstep += 1
+      self.pointer = self.nstep % self.bsize
 
       return numpy.reshape(Qnew, Q.shape)
