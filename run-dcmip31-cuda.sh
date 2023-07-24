fbb3681dd21197f9f5a244a99292fce7e2fbe12b
Brennan Lockinger (brl423) <brl423@usask.ca>
2023-07-24 10:57:05 -0600

    change run script to profile code

--- run-dcmip31-cuda.sh
+++ run-dcmip31-cuda.sh
@@ -3,5 +3,6 @@
 OMPI_MCA_opal_cuda_support=true \
 	CUDA_VISIBLE_DEVICES=1,2,3 \
 	mpiexec -n 6 \
-	python main_gef.py config/dcmip31_cuda.ini
+	python -m kernprof -l main_gef.py config/dcmip31_cuda.ini
 
+python -m line_profiler main_gef.py.lprof > main_gef.py.lprof.txt
