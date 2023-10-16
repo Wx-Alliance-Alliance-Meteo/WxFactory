b0d53585604e9bfa212c3c072a0e3796bf4ba0a0
Tanya Tafolla <ttafolla@rclogin01.cluster>
2023-10-16 10:28:32 -0700

    grouping 10k calls

---
+++ bat_files/por/call_10k.bat
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+cd /home/vma000/ords/gef_tanya
+. ./load_env.sh
+
+
+methods=("kiops" "pmex_1s" "pmex" "cwy_1s" "cwy_ne" "icwy_1s" "icwy_ne")
+order=(2 4 6)
+
+methodlen=${#methods[@]}
+orderlen=${#order[@]}
+
+#looping through each case and running it 7 times
+for ((n=0; n < $orderlen; n++)); do
+  for ((k=0; k < $methodlen; k++)); do
+     for ((j = 0; j < 7; j++)); do
+       mpirun -np 10000 python3 ./main_por.py ${order[${n}]} ${methods[${k}]}
+     done
+  done
+done
