9cf7bb4ba96be381daec279fea2a582d7151267b
Tanya Tafolla <ttafolla@rclogin01.cluster>
2023-10-16 10:11:02 -0700

    added .bat files for other stiff pde examples

---
+++ bat_files/burg/call_10k_epi2.bat
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cd /home/vma000/ords/gef_tanya
+. ./load_env.sh
+
+
+methods=("kiops" "pmex_1s" "pmex" "cwy_1s" "cwy_ne" "icwy_1s" "icwy_ne")
+
+
+methodlen=${#methods[@]}
+#looping through each case and running it 7 times
+for ((k=0; k < $methodlen; k++)); do
+   for ((j = 0; j < 7; j++)); do
+     mpirun -np 10000 python3 ./main_burg.py 2 ${methods[${k}]}
+  done
+done
