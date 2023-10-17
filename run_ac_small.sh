30f9469e6a5c6b5e263ad6ef34e5ee1383a31297
Tanya Tafolla <ttafolla@rclogin02.cluster>
2023-10-17 07:32:18 -0700

    grouping small and medium processor jobs

---
+++ run_ac_small.sh
@@ -0,0 +1,92 @@
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn ac_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/ac/call_100_epi2.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn ac_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/ac/call_100_epi4.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn ac_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/ac/call_100_epi6.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn ac_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/ac/call_400_epi2.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn ac_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/ac/call_400_epi4.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn ac_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/ac/call_400_epi6.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn ac_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/ac/call_625_epi2.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn ac_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/ac/call_625_epi4.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn ac_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/ac/call_625_epi6.bat     \
+           -listing $(pwd)/listings
+
+
+
+
+
+
+
+
+
+
+
+
+
+
