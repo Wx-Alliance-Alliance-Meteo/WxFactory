77e43f55d4a42314987e5febe53fdcf68e7d5649
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-10-18 17:37:12 -0700

    bat files for adr pde

--- bat_files/adr/call_100_epi4.bat
+++ bat_files/adr/call_100_epi4.bat
@@ -3,13 +3,13 @@ cd /home/vma000/ords/gef_tanya
 . ./load_env.sh
 
 
-methods=("kiops" "pmex_1s" "pmex" "cwy_1s" "cwy_ne" "icwy_1s" "icwy_ne")
+methods=("kiops" "pmex_1s" "pmex" "cwy_1s")
 
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
    for ((j = 0; j < 7; j++)); do
-     mpirun -np 10000 python3 ./main_adr.py 4 ${methods[${k}]}
+     mpirun -np 100 python3 ./main_adr.py 4 ${methods[${k}]}
   done
 done
---
+++ bat_files/adr/call_100_epi6_partB.bat
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cd /home/vma000/ords/gef_tanya
+. ./load_env.sh
+
+
+methods=("cwy_ne" "icwy_1s" "icwy_ne" "cwy_1s")
+
+
+methodlen=${#methods[@]}
+#looping through each case and running it 7 times
+for ((k=0; k < $methodlen; k++)); do
+   for ((j = 0; j < 7; j++)); do
+     mpirun -np 100 python3 ./main_adr.py 6 ${methods[${k}]}
+  done
+done
