dc873589118537986d0ad49cb539cee25c402f80
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-10-18 17:46:48 -0700

    adding main file for Advection-Diffusion-Reaction PDE

--- main_adr.py
+++ main_adr.py
@@ -1,21 +1,12 @@
 """
- This is the main file for running the Allen-Cahn PDE.
- u_t = alpha(u_xx + u_yy) - u + u^3
- x,y in [-1,1] with homogeneous Neumann boundary conditions.
-
- Integrating from t in [0, 0.02] with dt = 0.001; only 
- taking 20 steps. 
-
- Run with p = [100, 400, 625, 1600, 2500, 10k ] for 
- strong scaling results. 
-
- Runtime will be saved for each integrator and ortho method.  
+ This is the main file for running the Advection-Diffusion-Reaction
+ pde. 
 
 """
 
 import numpy as np
-import math
 import sys
+import math
 
 from mpi4py      import MPI
 from stiff_pdes  import JTV, initWorld, rhs_jac_pdefuncs, print_stuff
@@ -24,7 +15,7 @@ from time        import time
 
 #1. initialize world
 comm  = MPI.COMM_WORLD
-world = initWorld.InitWorld(comm, "Neumann", [-1.0, 1.0], 5002)
+world = initWorld.InitWorld(comm, "Neumann", [0.0, 1.0], 5002)
 
 #2. read in command line arguments
 epi_order    = int(sys.argv[1])
@@ -41,33 +32,31 @@ for j in range(0,world.numPointsY):      #y
     xcoord = world.startX + (k * world.dx)
     idx = j*world.numPointsY + k
  
-    Q[idx] = 0.1 + 0.1*math.cos(2.0*math.pi*xcoord)*math.cos(2.0*math.pi*ycoord)
+    Q[idx] = 0.3 + 256.0*(xcoord*ycoord *( 1 - xcoord)*(1-ycoord))**2
 
 #4. set up problem parameters
-#NOTE: in order for the lambda Jacobian mat_vec handle to be consistent
-#accross all pdes, coefficients need to be passed for diffusion, adv, react
-epsilon = 0.1  #coeff for diffusion
-alpha   = 0.0  #coeff for advection
-gamma   = 0.0  #coeff for reaction
+epsilon = 0.01   #coeff for diffusion
+alpha   = -6.0   #coeff for advection
+gamma   = 100.0  #coeff for reaction
 
 #5. set up integrator
 #access to functions that are inputs for EPI constructor
-rhs = rhs_jac_pdefuncs.allencahn_rhs
-jtv = rhs_jac_pdefuncs.allencahn_jtv
+rhs = rhs_jac_pdefuncs.adr_rhs
+jtv = rhs_jac_pdefuncs.adr_jtv
 
 #lamdba function of rhs, that way when called inside the integrator
 #the coefficients and world info don't have to be consistently passed
 #as an argument
-rhs_handle = lambda u: rhs(u, epsilon, world)
+rhs_handle = lambda u: rhs(u, epsilon, alpha, gamma, world)
 
 stepper = epi_for_others.Epi_others(epi_order, rhs_handle, jtv, ortho_method, world, [epsilon, alpha, gamma], init_substeps=10)
 
 #6. set up time integration
-#possitle dt from paper: 0.5, 0.25, 0.1250, 0.0625, 0.03125
-#dt = 0.000625 works!, 0.001 works!
+#possitle dt from paper: 0.01, 0.005, 0.0025, 0.00125, 6.25e-04
+#0.00001 works, 0.0001 works 
 t_start     = 0.0
 t_end       = 0.02
-dt          = 0.001
+dt          = 0.001 
 total_steps = math.ceil(t_end/dt)
 
 #7. time step
@@ -87,20 +76,17 @@ while(step < total_steps):
    if MPI.COMM_WORLD.rank == 0: print(f'\nStep {step} of {total_steps}')
 
    Q = stepper.step(Q, dt)
-
+  
 total_time = time() - start_time
 
-#if world.IamRoot:
-#   print("Total runtime = {}".format(total_time))
-   
-#print out stats
 if world.IamRoot:
+   #print("total_time = {}".format(total_time))
    size        = MPI.COMM_WORLD.Get_size()
    method      = str(epi_order)
    methodOrtho = str(ortho_method)
-   totaltime_name = "results_tanya/runtime_"+ methodOrtho + "_n" +  str(size) + "_e" + str(method) + "_ac.txt"
-
-"""   
+   totaltime_name = "results_tanya/runtime_"+ methodOrtho + "_n" +  str(size) + "_e" + str(method) + "_adr.txt"
+ 
+"""
 #print final solution 
 print("Gather solution")
 finalSolQ = MPI.COMM_WORLD.gather(Q, root=0)
@@ -108,6 +94,6 @@ finalSolQ = MPI.COMM_WORLD.gather(Q, root=0)
 if world.IamRoot:
 
    #1. gather solin 1 vec and print output file
-   filename = "epi5_ac_ts160.txt"
+   filename = "epi5_adr_ts100.txt"
    totalOutLap = print_stuff.print_sol(finalSolQ, filename, world)
 """
