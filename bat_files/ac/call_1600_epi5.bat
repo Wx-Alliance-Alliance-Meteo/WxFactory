2cdb36caa376a5cfd9b6b2e92fd34bb73c9297e6
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-11-22 09:21:42 -0800

    changed .bat files to epi5

--- bat_files/ac/call_1600_epi5.bat
+++ bat_files/ac/call_1600_epi5.bat
@@ -10,6 +10,6 @@ methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
    for ((j = 0; j < 7; j++)); do
-     mpirun -np 10000 python3 ./main_ac.py 2 ${methods[${k}]}
+     mpirun -np 1600 python3 ./main_ac.py 5 ${methods[${k}]}
   done
 done
