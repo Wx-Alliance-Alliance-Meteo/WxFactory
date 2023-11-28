e1932f8d58d703d280e1b044b71b26c589698b28
Tanya Tafolla <ttafolla@rclogin02.cluster>
2023-11-28 14:24:43 -0800

    organized srerk for 1k procs

--- run_1k_gef.sh
+++ run_1k_gef.sh
@@ -1,12 +1,90 @@
+#----------------epi 4 5 6 again-------------------------
 
-#----------------------1176---------------------------------
+#-----------epi4 --------------------
+ord_soumet -cpus 1176                                \
+           -w 180                                    \
+           -jn gef_1176                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e4_1176_1s.bat      \
+           -listing $(pwd)/listings
 
 ord_soumet -cpus 1176                                \
            -w 180                                    \
            -jn gef_1176                         \
            -mpi                                      \
            -share e                                  \
-           -jobfile bat_files/call_1176_epi.bat      \
+           -jobfile bat_files/finalrun/call_e4_1176_nev1.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 1176                                \
+           -w 180                                    \
+           -jn gef_1176                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e4_1176_nev2.bat      \
+           -listing $(pwd)/listings
+
+#----------epi 5---------------------
+ord_soumet -cpus 1176                                \
+           -w 180                                    \
+           -jn gef_1176                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e5_1176_1s.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 1176                                \
+           -w 180                                    \
+           -jn gef_1176                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e5_1176_nev1.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 1176                                \
+           -w 180                                    \
+           -jn gef_1176                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e5_1176_nev2.bat      \
+           -listing $(pwd)/listings
+
+
+
+#----------epi 6---------------------
+ord_soumet -cpus 1176                                \
+           -w 180                                    \
+           -jn gef_1176                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e6_1176_1s.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 1176                                \
+           -w 180                                    \
+           -jn gef_1176                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e6_1176_nev1.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 1176                                \
+           -w 180                                    \
+           -jn gef_1176                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e6_1176_nev2.bat      \
+           -listing $(pwd)/listings
+
+#----------------------srerk---------------------------------
+
+ord_soumet -cpus 1176                              \
+           -w 180                                  \
+           -jn gef_1176                            \
+           -mpi                                    \
+           -share e                                \
+           -jobfile bat_files/call_1176_s3_1sv1.bat  \
            -listing $(pwd)/listings
 
 ord_soumet -cpus 1176                              \
@@ -14,23 +92,16 @@ ord_soumet -cpus 1176                              \
            -jn gef_1176                            \
            -mpi                                    \
            -share e                                \
-           -jobfile bat_files/call_1176_srpt1.bat  \
+           -jobfile bat_files/call_1176_s3_1sv2.bat  \
            -listing $(pwd)/listings
 
-ord_soumet -cpus 1176                               \
-           -w 180                                   \
-           -jn gef_1176                             \
-           -mpi                                     \
-           -share e                                 \
-           -jobfile bat_files/call_1176_srpt2.bat   \
-           -listing $(pwd)/listings
 
-ord_soumet -cpus 1176                               \
-           -w 180                                   \
-           -jn gef_1176                             \
-           -mpi                                     \
-           -share e                                 \
-           -jobfile bat_files/call_1176_srpt3.bat   \
+ord_soumet -cpus 1176                              \
+           -w 180                                  \
+           -jn gef_1176                            \
+           -mpi                                    \
+           -share e                                \
+           -jobfile bat_files/call_1176_s3_1sv3.bat  \
            -listing $(pwd)/listings
 
 ord_soumet -cpus 1176                              \
@@ -38,24 +109,72 @@ ord_soumet -cpus 1176                              \
            -jn gef_1176                            \
            -mpi                                    \
            -share e                                \
-           -jobfile bat_files/call_1176_srpt4.bat  \
+           -jobfile bat_files/call_1176_s3_nev1.bat  \
            -listing $(pwd)/listings
 
-ord_soumet -cpus 1176                               \
-           -w 180                                   \
-           -jn gef_1176                             \
-           -mpi                                     \
-           -share e                                 \
-           -jobfile bat_files/call_1176_srpt5.bat   \
-           -listing $(pwd)/listings
-
-ord_soumet -cpus 1176                               \
-           -w 180                                   \
-           -jn gef_1176                             \
-           -mpi                                     \
-           -share e                                 \
-           -jobfile bat_files/call_1176_srpt6.bat   \
+ord_soumet -cpus 1176                              \
+           -w 180                                  \
+           -jn gef_1176                            \
+           -mpi                                    \
+           -share e                                \
+           -jobfile bat_files/call_1176_s3_nev2.bat  \
            -listing $(pwd)/listings
 
 
+ord_soumet -cpus 1176                              \
+           -w 180                                  \
+           -jn gef_1176                            \
+           -mpi                                    \
+           -share e                                \
+           -jobfile bat_files/call_1176_s3_nev3.bat  \
+           -listing $(pwd)/listings
 
+ord_soumet -cpus 1176                              \
+           -w 180                                  \
+           -jn gef_1176                            \
+           -mpi                                    \
+           -share e                                \
+           -jobfile bat_files/call_1176_s6_1sv1.bat  \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 1176                              \
+           -w 180                                  \
+           -jn gef_1176                            \
+           -mpi                                    \
+           -share e                                \
+           -jobfile bat_files/call_1176_s6_1sv2.bat  \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 1176                              \
+           -w 180                                  \
+           -jn gef_1176                            \
+           -mpi                                    \
+           -share e                                \
+           -jobfile bat_files/call_1176_s6_1sv3.bat  \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 1176                              \
+           -w 180                                  \
+           -jn gef_1176                            \
+           -mpi                                    \
+           -share e                                \
+           -jobfile bat_files/call_1176_s6_nev1.bat  \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 1176                              \
+           -w 180                                  \
+           -jn gef_1176                            \
+           -mpi                                    \
+           -share e                                \
+           -jobfile bat_files/call_1176_s6_nev2.bat  \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 1176                              \
+           -w 180                                  \
+           -jn gef_1176                            \
+           -mpi                                    \
+           -share e                                \
+           -jobfile bat_files/call_1176_s6_nev3.bat  \
+           -listing $(pwd)/listings
