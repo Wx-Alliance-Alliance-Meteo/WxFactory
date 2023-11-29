c38d16197792bad5fa4ae9d3b88a250282d25c5c
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-11-29 10:09:31 -0800

    split up calls to srerk because did not finish last time

---
+++ bat_files/call_2646_s3_1s.bat
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd /home/vma000/code/gef_tanya
+. ./load_env.sh
+
+methods=("case5_kiops.ini" "case5_pmex1s.ini" "case5_icwy1s.ini" "case6_kiops.ini" "case6_pmex1s.ini" "case6_icwy1s.ini" "galewsky_kiops.ini" "galewsky_pmex1s.ini" "galewsky_icwy1s.ini" "case5_cwy1s.ini" "case6_cwy1s.ini" "galewsky_cwy1s.ini")
+
+
+methodlen=${#methods[@]}
+#looping through each case and running it 7 times
+for ((k=0; k < $methodlen; k++)); do
+   for ((j = 0; j < 7; j++)); do
+     mpirun -np 2646 python3 ./main_gef.py config/test_files/srerk3/${methods[${k}]}
+  done
+done
--- run_2k_gef.sh
+++ run_2k_gef.sh
@@ -1,13 +1,82 @@
+#----------------run epi 4 5 6 again------------------
 
-#----------------------2646---------------------------------
+
+#----------epi4 ------------
+ord_soumet -cpus 2646                                \
+           -w 180                                    \
+           -jn gef_2646                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e4_2k_1s.bat      \
+           -listing $(pwd)/listings
 
 ord_soumet -cpus 2646                                \
            -w 180                                    \
            -jn gef_2646                         \
            -mpi                                      \
            -share e                                  \
-           -jobfile bat_files/call_2646_epi.bat      \
+           -jobfile bat_files/finalrun/call_e4_2k_nev1.bat      \
            -listing $(pwd)/listings
+
+ord_soumet -cpus 2646                                \
+           -w 180                                    \
+           -jn gef_2646                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e4_2k_nev2.bat      \
+           -listing $(pwd)/listings
+
+#----------epi5 ------------
+ord_soumet -cpus 2646                                \
+           -w 180                                    \
+           -jn gef_2646                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e5_2k_1s.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 2646                                \
+           -w 180                                    \
+           -jn gef_2646                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e5_2k_nev1.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 2646                                \
+           -w 180                                    \
+           -jn gef_2646                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e5_2k_nev2.bat      \
+           -listing $(pwd)/listings
+
+#----------epi6 ------------
+ord_soumet -cpus 2646                                \
+           -w 180                                    \
+           -jn gef_2646                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e6_2k_1s.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 2646                                \
+           -w 180                                    \
+           -jn gef_2646                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e6_2k_nev1.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 2646                                \
+           -w 180                                    \
+           -jn gef_2646                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e6_2k_nev2.bat      \
+           -listing $(pwd)/listings
+
+#----------------------srerk---------------------------------
 
 ord_soumet -cpus 2646                              \
            -w 180                                  \
@@ -30,9 +99,16 @@ ord_soumet -cpus 2646                               \
            -jn gef_2646                             \
            -mpi                                     \
            -share e                                 \
-           -jobfile bat_files/call_2646_srerk.bat   \
+           -jobfile bat_files/call_2646_s3_1s.bat   \
            -listing $(pwd)/listings
 
+ord_soumet -cpus 2646                               \
+           -w 180                                   \
+           -jn gef_2646                             \
+           -mpi                                     \
+           -share e                                 \
+           -jobfile bat_files/call_2646_s6_1s.bat   \
+           -listing $(pwd)/listings
 
 
 
