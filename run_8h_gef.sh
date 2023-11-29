87ec1ce4929c0bc2450edb71a0b62a778977103d
Tanya Tafolla <ttafolla@rclogin01.cluster>
2023-11-29 10:40:59 -0800

    added runs that did not finish

--- run_8h_gef.sh
+++ run_8h_gef.sh
@@ -1,13 +1,107 @@
+#---------------------running epi 4, 5, 6 again------------
 
-#----------------------864---------------------------------
+#----------epi 4------------
+ord_soumet -cpus 864                                \
+           -w 180                                    \
+           -jn gef_864                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e4_864_1sv1.bat      \
+           -listing $(pwd)/listings
 
 ord_soumet -cpus 864                                \
            -w 180                                    \
            -jn gef_864                         \
            -mpi                                      \
            -share e                                  \
-           -jobfile bat_files/call_864_epi.bat      \
+           -jobfile bat_files/finalrun/call_e4_864_1sv2.bat      \
            -listing $(pwd)/listings
+
+ord_soumet -cpus 864                                \
+           -w 180                                    \
+           -jn gef_864                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e4_864_nev1.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                                \
+           -w 180                                    \
+           -jn gef_864                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e4_864_nev2.bat      \
+           -listing $(pwd)/listings
+
+#----------epi 5------------
+ord_soumet -cpus 864                                \
+           -w 180                                    \
+           -jn gef_864                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e5_864_1sv1.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                                \
+           -w 180                                    \
+           -jn gef_864                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e5_864_1sv2.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                                \
+           -w 180                                    \
+           -jn gef_864                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e5_864_nev1.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                                \
+           -w 180                                    \
+           -jn gef_864                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e5_864_nev2.bat      \
+           -listing $(pwd)/listings
+
+#----------epi 6------------
+ord_soumet -cpus 864                                \
+           -w 180                                    \
+           -jn gef_864                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e6_864_1sv1.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                                \
+           -w 180                                    \
+           -jn gef_864                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e6_864_1sv2.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                                \
+           -w 180                                    \
+           -jn gef_864                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e6_864_nev1.bat      \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                                \
+           -w 180                                    \
+           -jn gef_864                         \
+           -mpi                                      \
+           -share e                                  \
+           -jobfile bat_files/finalrun/call_e6_864_nev2.bat      \
+           -listing $(pwd)/listings
+
+
+
+#----------------------srerk---------------------------------
 
 ord_soumet -cpus 864                              \
            -w 180                                  \
@@ -57,5 +151,60 @@ ord_soumet -cpus 864                               \
            -jobfile bat_files/call_864_srpt6.bat   \
            -listing $(pwd)/listings
 
+ord_soumet -cpus 864                               \
+           -w 180                                   \
+           -jn gef_864                             \
+           -mpi                                     \
+           -share e                                 \
+           -jobfile bat_files/call_864_s6_1sv1.bat   \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                               \
+           -w 180                                   \
+           -jn gef_864                             \
+           -mpi                                     \
+           -share e                                 \
+           -jobfile bat_files/call_864_s6_1sv2.bat   \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                               \
+           -w 180                                   \
+           -jn gef_864                             \
+           -mpi                                     \
+           -share e                                 \
+           -jobfile bat_files/call_864_s6_1sv3.bat   \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                               \
+           -w 180                                   \
+           -jn gef_864                             \
+           -mpi                                     \
+           -share e                                 \
+           -jobfile bat_files/call_864_s3_1sv1.bat   \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                               \
+           -w 180                                   \
+           -jn gef_864                             \
+           -mpi                                     \
+           -share e                                 \
+           -jobfile bat_files/call_864_s3_1sv2.bat   \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                               \
+           -w 180                                   \
+           -jn gef_864                             \
+           -mpi                                     \
+           -share e                                 \
+           -jobfile bat_files/call_864_s3_1sv3.bat   \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 864                               \
+           -w 180                                   \
+           -jn gef_864                             \
+           -mpi                                     \
+           -share e                                 \
+           -jobfile bat_files/call_864_s3_nev1.bat   \
+           -listing $(pwd)/listings
 
 
