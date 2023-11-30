55e0a1ad8fa101526c04ce9f3af84a813817edca
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-11-30 11:10:12 -0800

    ac with srerk calls

--- bat_files/ac/call_100_s3.bat
+++ bat_files/ac/call_100_s3.bat
@@ -10,6 +10,6 @@ methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
    for ((j = 0; j < 7; j++)); do
-     mpirun -np 100 python3 ./main_ac.py epi5 ${methods[${k}]}
+     mpirun -np 100 python3 ./main_ac.py srerk3 ${methods[${k}]}
   done
 done
