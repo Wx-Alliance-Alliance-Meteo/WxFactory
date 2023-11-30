7ffe231b0fbf21811259fbe29639ce9409ef0c35
Tanya Tafolla <ttafolla@rclogin01.cluster>
2023-11-30 09:18:51 -0800

    removed epi 4,6 from 10k and 4k calls

--- bat_files/finalrun/call_epi_4k_1sv1.bat
+++ bat_files/finalrun/call_epi_4k_1sv1.bat
@@ -3,7 +3,8 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("epi4/case5_pmex1s.ini" "epi4/case5_cwy1s.ini" "epi4/case5_icwy1s.ini" "epi4/case5_kiops.ini" "epi4/case6_pmex1s.ini" "epi4/case6_cwy1s.ini" "epi4/case6_icwy1s.ini" "epi4/case6_kiops.ini" "epi5/case5_pmex1s.ini" "epi5/case5_cwy1s.ini" "epi5/case5_icwy1s.ini" "epi5/case5_kiops.ini" "epi5/case6_pmex1s.ini" "epi5/case6_cwy1s.ini" "epi5/case6_icwy1s.ini" "epi5/case6_kiops.ini" "epi6/case6_pmex1s.ini" "epi6/case6_cwy1s.ini" "epi6/case6_icwy1s.ini")
+methods=("epi4/case5_kiops.ini" "epi4/case6_kiops.ini" "epi4/galewsky_kiops.ini" "epi5/case5_pmex1s.ini" "epi5/case5_cwy1s.ini" "epi5/case5_icwy1s.ini" "epi5/case5_kiops.ini" "epi5/case6_pmex1s.ini" "epi5/case6_cwy1s.ini" "epi5/case6_icwy1s.ini" "epi5/case6_kiops.ini" "epi5/galewsky_pmex1s.ini" "epi5/galewsky_cwy1s.ini" "epi5/galewsky_icwy1s.ini" "epi5/galewsky_kiops.ini" "epi6/case5_kiops.ini" "epi6/case6_kiops.ini" "epi6/galewsky_kiops.ini")
+
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
