6573701cfedb89ba1f237889931a7efe7a16f3e4
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-11-30 11:35:11 -0800

    fixed the processor size

--- bat_files/ac/call_400_epi5.bat
+++ bat_files/ac/call_400_epi5.bat
@@ -10,6 +10,6 @@ methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
    for ((j = 0; j < 7; j++)); do
-     mpirun -np 500 python3 ./main_ac.py epi5 ${methods[${k}]}
+     mpirun -np 400 python3 ./main_ac.py epi5 ${methods[${k}]}
   done
 done
