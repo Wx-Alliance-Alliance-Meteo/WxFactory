72d04f2537dffeb7d3f87438823f25c18ae508e7
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-11-30 11:49:45 -0800

    AC with p625

---
+++ bat_files/ac/call_625_s3_ne.bat
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cd /home/vma000/code/gef_tanya
+. ./load_env.sh
+
+
+methods=("pmex" "icwy_ne" "cwy_ne" "cwy_1s")
+
+
+methodlen=${#methods[@]}
+#looping through each case and running it 7 times
+for ((k=0; k < $methodlen; k++)); do
+   for ((j = 0; j < 7; j++)); do
+     mpirun -np 625 python3 ./main_ac.py srerk3 ${methods[${k}]}
+  done
+done
