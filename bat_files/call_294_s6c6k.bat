11d97ab1f0d6381b1694c1c386a1264c312f9106
Tanya Tafolla <ttafolla@rclogin01.cluster>
2023-11-30 07:36:56 -0800

    adding 1s srerk methods

---
+++ bat_files/call_294_s6c6k.bat
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd /home/vma000/code/gef_tanya
+. ./load_env.sh
+
+#looping through each case and running it 7 times
+for ((j = 0; j < 7; j++)); do
+   mpirun -np 294 python3 ./main_gef.py config/test_files/srerk6/case6_kiops.ini
+done
