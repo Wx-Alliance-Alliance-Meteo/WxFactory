ebed0bc88f4ec5cce404308029c61eec5d1d6bf9
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-11-30 11:38:37 -0800

    AC with p2500

--- bat_files/ac/call_2500_s6.bat
+++ bat_files/ac/call_2500_s6.bat
@@ -10,6 +10,6 @@ methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
    for ((j = 0; j < 7; j++)); do
-     mpirun -np 2500 python3 ./main_ac.py epi6 ${methods[${k}]}
+     mpirun -np 2500 python3 ./main_ac.py srerk6 ${methods[${k}]}
   done
 done
