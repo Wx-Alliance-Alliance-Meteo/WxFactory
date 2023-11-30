e9d10462c092cd57f2277bbb39e61847366a6d24
Tanya Tafolla <ttafolla@rclogin01.cluster>
2023-11-30 07:43:41 -0800

    grouping 1s calls

--- bat_files/call_294_s6c8k.bat
+++ bat_files/call_294_s6c8k.bat
@@ -2,7 +2,11 @@
 cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
+methods=("galewsky_kiops.ini" "galewsky_cwy1s.ini")
+
 #looping through each case and running it 7 times
-for ((j = 0; j < 7; j++)); do
-   mpirun -np 294 python3 ./main_gef.py config/test_files/srerk6/galewski_kiops.ini
+for ((k=0; k < $methodlen; k++)); do
+   for ((j = 0; j < 7; j++)); do
+     mpirun -np 294 python3 ./main_gef.py config/test_files/srerk6/${methods[${k}]}
+  done
 done
