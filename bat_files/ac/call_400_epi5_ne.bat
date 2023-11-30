c81a5ecce6517d8c0c38abea51283cfe79633331
Tanya Tafolla <ttafolla@rclogin03.merced>
2023-11-30 11:33:13 -0800

    AC with p400

--- bat_files/ac/call_400_epi5_ne.bat
+++ bat_files/ac/call_400_epi5_ne.bat
@@ -3,13 +3,13 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("kiops" "pmex_1s" "pmex" "cwy_1s" "cwy_ne" "icwy_1s" "icwy_ne")
+methods=("pmex" "icwy_ne" "cwy_ne" "cwy_1s")
 
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
    for ((j = 0; j < 7; j++)); do
-     mpirun -np 625 python3 ./main_ac.py 5 ${methods[${k}]}
+     mpirun -np 400 python3 ./main_ac.py epi5 ${methods[${k}]}
   done
 done
--- bat_files/ac/call_400_epi6.bat
+++ bat_files/ac/call_400_epi6.bat
@@ -3,13 +3,13 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("kiops" "pmex_1s" "pmex" "cwy_1s" "cwy_ne" "icwy_1s" "icwy_ne")
+methods=("kiops" "pmex_1s" "icwy_1s" "cwy_1s")
 
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
    for ((j = 0; j < 7; j++)); do
-     mpirun -np 400 python3 ./main_ac.py 6 ${methods[${k}]}
+     mpirun -np 400 python3 ./main_ac.py epi6 ${methods[${k}]}
   done
 done
