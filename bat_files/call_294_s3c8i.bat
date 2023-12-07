d56ddc1f278afb547c4f6c380146d261bee37ed1
Vincent Magnoux <vincent.magnoux@ec.gc.ca>
2023-12-07 20:45:19 +0000

    Fixed missing variable in some batch scripts

--- bat_files/call_294_s3c8i.bat
+++ bat_files/call_294_s3c8i.bat
@@ -4,6 +4,7 @@ cd /home/vma000/code/gef_tanya
 
 methods=("galewsky_icwy1s.ini" "galewsky_cwy1s.ini")
 
+methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
    for ((j = 0; j < 7; j++)); do
