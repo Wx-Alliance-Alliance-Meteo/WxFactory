23a93114752f2694d4447736d6d3fb4775cc3ad2
tanya.tafolla <ttafolla@ucmerced.edu>
2024-01-08 08:53:21 -0800

    changed ne to ne1s methods

---
+++ bat_files/ac/call_625_epi_ne.bat
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+cd /home/vma000/code/gef_tanya
+. ./load_env.sh
+
+
+methods=("pmex_ne1s" "cwy_ne1s" "icwy_ne1s")
+integrator=("epi4" "epi5" "epi6")
+
+methodlen=${#methods[@]}
+intlen=${#integrator[@]}
+
+#looping through each case and running it 7 times
+for ((n=0; n < $intlen; n++)); do
+  for ((k=0; k < $methodlen; k++)); do
+     for ((j = 0; j < 7; j++)); do
+       mpirun -np 625 python3 ./main_ac.py ${integrator[${n}]} ${methods[${k}]}
+    done
+  done
+done
