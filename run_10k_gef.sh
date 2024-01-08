e0630821195d81513627164feabd9e92b151f91c
tanya.tafolla <ttafolla@ucmerced.edu>
2024-01-08 10:00:04 -0800

    updated .sh files for missing test

--- run_10k_gef.sh
+++ run_10k_gef.sh
@@ -1,53 +1,11 @@
 
 
 ord_soumet -cpus 10584                                \
-           -w 90                                    \
-           -jn gef_10584                            \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/finalrun/call_epi_10k_1s.bat      \
-           -listing $(pwd)/listings
-
-ord_soumet -cpus 10584                                \
            -w 180                                    \
            -jn gef_10584                            \
            -mpi                                      \
            -share e                                  \
            -jobfile bat_files/finalrun/call_epi_10k_ne.bat      \
            -listing $(pwd)/listings
-
-
-ord_soumet -cpus 10584                                \
-           -w 180                                    \
-           -jn gef_10584                            \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/call_10k_s3_1s.bat      \
-           -listing $(pwd)/listings
-
-ord_soumet -cpus 10584                                \
-           -w 180                                    \
-           -jn gef_10584                            \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/call_10k_s3_ne.bat      \
-           -listing $(pwd)/listings
-
-ord_soumet -cpus 10584                                \
-           -w 180                                    \
-           -jn gef_10584                            \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/call_10k_s6_1s.bat      \
-           -listing $(pwd)/listings
-
-ord_soumet -cpus 10584                                \
-           -w 180                                    \
-           -jn gef_10584                            \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/call_10k_s6_ne.bat      \
-           -listing $(pwd)/listings
-
 
 
