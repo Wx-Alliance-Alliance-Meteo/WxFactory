2af1a731ad4d1e8cc4d2d441b895363c7de02ce1
tanya.tafolla <ttafolla@ucmerced.edu>
2024-01-08 09:29:06 -0800

    for rerunning

--- bat_files/finalrun/call_epi_10k_ne.bat
+++ bat_files/finalrun/call_epi_10k_ne.bat
@@ -3,7 +3,9 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("epi4/case5_pmexne.ini" "epi4/case5_cwyne.ini" "epi4/case5_icwyne.ini" "epi4/case6_pmexne.ini" "epi4/case6_cwyne.ini" "epi4/case6_icwyne.ini" "epi4/galewsky_pmexne.ini" "epi4/galewsky_cwyne.ini" "epi4/galewsky_icwyne.ini" "epi5/case5_pmexne.ini" "epi5/case5_cwyne.ini" "epi5/case5_icwyne.ini" "epi5/case6_pmexne.ini" "epi5/case6_cwyne.ini" "epi5/case6_icwyne.ini" "epi5/galewsky_pmexne.ini" "epi5/galewsky_cwyne.ini" "epi5/galewsky_icwyne.ini" "epi6/case5_pmexne.ini" "epi6/case5_cwyne.ini" "epi6/case5_icwyne.ini" "epi6/case6_pmexne.ini" "epi6/case6_cwyne.ini" "epi6/case6_icwyne.ini" "epi6/galewsky_pmexne.ini" "epi6/galewsky_cwyne.ini" "epi6/galewsky_icwyne.ini")
+#methods=("epi4/case5_pmexne.ini" "epi4/case5_cwyne.ini" "epi4/case5_icwyne.ini" "epi4/case6_pmexne.ini" "epi4/case6_cwyne.ini" "epi4/case6_icwyne.ini" "epi4/galewsky_pmexne.ini" "epi4/galewsky_cwyne.ini" "epi4/galewsky_icwyne.ini" "epi5/case5_pmexne.ini" "epi5/case5_cwyne.ini" "epi5/case5_icwyne.ini" "epi5/case6_pmexne.ini" "epi5/case6_cwyne.ini" "epi5/case6_icwyne.ini" "epi5/galewsky_pmexne.ini" "epi5/galewsky_cwyne.ini" "epi5/galewsky_icwyne.ini" "epi6/case5_pmexne.ini" "epi6/case5_cwyne.ini" "epi6/case5_icwyne.ini" "epi6/case6_pmexne.ini" "epi6/case6_cwyne.ini" "epi6/case6_icwyne.ini" "epi6/galewsky_pmexne.ini" "epi6/galewsky_cwyne.ini" "epi6/galewsky_icwyne.ini")
+
+methods=("epi5/case5_pmex1s.ini" "epi5/case6_pmex1s.ini" "epi5/case5_icwy1s.ini" "epi5/case6_icwy1s.ini" "epi5/case5_cwy1s.ini" "epi5/case6_cwy1s.ini" "epi6/galewsky_icwyne.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
