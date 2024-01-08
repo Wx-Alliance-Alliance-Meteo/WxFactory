604017352e921ab32ef35db7ac48ad174368355c
tanya.tafolla <ttafolla@ucmerced.edu>
2024-01-08 09:50:41 -0800

    rerunning tests

--- bat_files/finalrun/call_e4_1176_nev2.bat
+++ bat_files/finalrun/call_e4_1176_nev2.bat
@@ -3,7 +3,8 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("epi4/case6_icwyne.ini" "epi4/galewsky_pmexne.ini" "epi4/galewsky_cwyne.ini" "epi4/galewsky_icwyne.ini")
+#methods=("epi4/case6_icwyne.ini" "epi4/galewsky_pmexne.ini" "epi4/galewsky_cwyne.ini" "epi4/galewsky_icwyne.ini")
+methods=("epi5/galewsky_kiops.ini" "epi6/case5_cwyne.ini" "epi6/case6_cwyne.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
