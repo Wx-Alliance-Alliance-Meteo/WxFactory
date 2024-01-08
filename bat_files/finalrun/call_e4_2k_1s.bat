b8d752726915bdb8b9eee2a457d4595223d20e4e
tanya.tafolla <ttafolla@ucmerced.edu>
2024-01-08 09:55:43 -0800

    rerunning missing test

--- bat_files/finalrun/call_e4_2k_1s.bat
+++ bat_files/finalrun/call_e4_2k_1s.bat
@@ -3,7 +3,9 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("epi4/case5_pmex1s.ini" "epi4/case5_cwy1s.ini" "epi4/case5_icwy1s.ini" "epi4/case5_kiops.ini" "epi4/case6_pmex1s.ini" "epi4/case6_cwy1s.ini" "epi4/case6_icwy1s.ini" "epi4/case6_kiops.ini" "epi4/galewsky_pmex1s.ini" "epi4/galewsky_cwy1s.ini" "epi4/galewsky_icwy1s.ini" "epi4/galewsky_kiops.ini")
+#methods=("epi4/case5_pmex1s.ini" "epi4/case5_cwy1s.ini" "epi4/case5_icwy1s.ini" "epi4/case5_kiops.ini" "epi4/case6_pmex1s.ini" "epi4/case6_cwy1s.ini" "epi4/case6_icwy1s.ini" "epi4/case6_kiops.ini" "epi4/galewsky_pmex1s.ini" "epi4/galewsky_cwy1s.ini" "epi4/galewsky_icwy1s.ini" "epi4/galewsky_kiops.ini")
+
+methods=("epi4/case5_icwy1s.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
