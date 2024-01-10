b6ca5bd2e4668830be6398de657a0f0a98d8f9c4
tanya.tafolla <ttafolla@ucmerced.edu>
2024-01-10 12:23:19 -0800

    fixed typeo from for-loops

--- bat_files/call_864_s6_1sv1.bat
+++ bat_files/call_864_s6_1sv1.bat
@@ -10,5 +10,5 @@ methodlen=${#methods[@]}
 for ((k=0; k < $methodlen; k++)); do
    for ((j = 0; j < 7; j++)); do
      mpirun -np 864 python3 ./main_gef.py config/test_files/${methods[${k}]}
-  do1s
-do1s
+  done
+done
--- bat_files/call_864_s6_1sv2.bat
+++ bat_files/call_864_s6_1sv2.bat
@@ -3,12 +3,12 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("srerk6/case6_pmex1s.ini" "srerk6/case6_cwy1s.ini" "srerk6/case6_kiops.ini" "srerk6/galewsky_icwy1s.ini")
+methods=("srerk6/case6_pmex1s.ini" "srerk6/case6_kiops.ini" "srerk6/case6_cwy1s.ini" "srerk6/galewsky_icwy1s.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
    for ((j = 0; j < 7; j++)); do
      mpirun -np 864 python3 ./main_gef.py config/test_files/${methods[${k}]}
-  do1s
-do1s
+  done
+done
