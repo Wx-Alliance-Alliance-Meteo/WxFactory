5c5f1c83c158b4a65e479e659ef84b2bb0769cd7
tanya.tafolla <ttafolla@ucmerced.edu>
2024-01-10 12:33:16 -0800

    fixed typo in sw 8h calls

--- run_8h_gef.sh
+++ run_8h_gef.sh
@@ -48,3 +48,10 @@ ord_soumet -cpus 864                               \
            -jobfile bat_files/call_864_s3_1sv3.bat   \
            -listing $(pwd)/listings
 
+ord_soumet -cpus 864                               \
+           -w 180                                   \
+           -jn gef_864                             \
+           -mpi                                     \
+           -share e                                 \
+           -jobfile bat_files/call_864_s3_1sv4.bat   \
+           -listing $(pwd)/listings
