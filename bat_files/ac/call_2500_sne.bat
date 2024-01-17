f77b3e4d1ecf93fd6a19db503a48b97c6ae84e1b
Tanya Tafolla <ttafolla@rclogin03.merced>
2024-01-17 10:41:39 -0800

    added ne methods to srerk

---
+++ bat_files/ac/call_2500_sne.bat
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+cd /home/vma000/code/gef_tanya
+. ./load_env.sh
+
+
+methods=("pmex" "cwy_ne" "icwy_ne")
+integrators=("srerk3" "srerk6")
+
+methodlen=${#methods[@]}
+intlen=${#integrators[@]}
+#looping through each case and running it 7 times
+for ((n=0; n < $intlen; n++)); do 
+  for ((k=0; k < $methodlen; k++)); do
+     for ((j = 0; j < 7; j++)); do
+       mpirun -np 2500 python3 ./main_ac.py ${integrators[${n}]} ${methods[${k}]}
+    done
+  done
+done
