832699d2ba8c8858a994778c12c24119fbcf9947
Tanya Tafolla <ttafolla@rclogin03.merced>
2024-01-17 11:11:09 -0800

    added .bat files for p100 Burger's Eqn

---
+++ bat_files/burg/call_100_s3_ne.bat
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd /home/vma000/code/gef_tanya
+. ./load_env.sh
+
+
+methods=("pmex" "icwy_ne" "cwy_ne")
+
+methodlen=${#methods[@]}
+#looping through each case and running it 7 times
+for ((k=0; k < $methodlen; k++)); do
+   for ((j = 0; j < 7; j++)); do
+     mpirun -np 100 python3 ./main_burg.py srerk3 ${methods[${k}]}
+  done
+done
