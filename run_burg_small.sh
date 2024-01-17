1b96550b1dbe5413950f4531195e7b9d75e51481
Tanya Tafolla <ttafolla@rclogin02.cluster>
2024-01-17 12:22:42 -0800

    .sh file for running Burger's equation

---
+++ run_burg_small.sh
@@ -0,0 +1,392 @@
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_epi5_ne1s.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_epi4_ne1s.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_epi6_ne1s.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_s6_ne1s.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_s3_ne1s.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_epi5_ne.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_epi4_ne.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_epi6_ne.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_s6_ne.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_s3_ne.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_s6.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_s3.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_epi5.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_epi4.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 100                                  \
+           -w 180                                      \
+           -jn burg_100_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_100_epi6.bat     \
+           -listing $(pwd)/listings
+
+
+#-----------------------------------------------------------
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_epi5_ne1s.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_epi4_ne1s.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_epi6_ne1s.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_s3_ne1s.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_s6_ne1s.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_epi5_ne.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_epi4_ne.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_epi6_ne.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_s3_ne.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_s6_ne.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_s3.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_s6.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_epi5.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_epi4.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 400                                  \
+           -w 180                                      \
+           -jn burg_400_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_400_epi6.bat     \
+           -listing $(pwd)/listings
+#---------------------------------------------------
+
+ord_soumet -cpus 625                                 \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_epi5_ne1s.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_epi4_ne1s.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_epi6_ne1s.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_s3_ne1s.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_s6_ne1s.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_epi5_ne.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_epi4_ne.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_epi6_ne.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_s3_ne.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_s6_ne.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_s3.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_s6.bat     \
+           -listing $(pwd)/listings
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_epi5.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_epi4.bat     \
+           -listing $(pwd)/listings
+
+
+ord_soumet -cpus 625                                  \
+           -w 180                                      \
+           -jn burg_625_stats                          \
+           -mpi                                        \
+           -share e                                    \
+           -jobfile bat_files/burg/call_625_epi6.bat     \
+           -listing $(pwd)/listings
+
+
