e462380f1ed0577699029ab9393c716ea6b94bf2
Tanya Tafolla <ttafolla@rclogin03.merced>
2024-01-17 09:10:31 -0800

    changed to ne1s methods

--- bat_files/adr/call_625_epi4.bat
+++ bat_files/adr/call_625_epi4.bat
@@ -3,13 +3,18 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("kiops" "pmex_1s" "pmex" "cwy_1s" "cwy_ne" "icwy_1s" "icwy_ne")
+#methods=("kiops" "pmex_1s" "pmex" "cwy_1s" "cwy_ne" "icwy_1s" "icwy_ne")
 
+methods=("pmex_ne1s" "icwy_ne1s" "cwy_ne1s")
+integrators=("epi4" "epi5" "epi6")
 
+intlen=${#integrators[@]}
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
-for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
-     mpirun -np 625 python3 ./main_adr.py epi4 ${methods[${k}]}
+for ((n=0; n < $intlen; n++)); do
+  for ((k=0; k < $methodlen; k++)); do
+     for ((j = 0; j < 7; j++)); do
+       mpirun -np 625 python3 ./main_adr.py ${integrators[${n}]} ${methods[${k}]}
+    done
   done
 done
--- bat_files/adr/call_625_s6.bat
+++ bat_files/adr/call_625_s6.bat
@@ -3,8 +3,7 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("kiops" "pmex_1s" "pmex" "cwy_1s" "cwy_ne" "icwy_1s" "icwy_ne")
-
+methods=("kiops" "pmex_1s" "pmex_ne1s" "icwy_1s" "icwy_ne1s" "cwy_1s" "cwy_ne1s")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
