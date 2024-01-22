473034fcf1b08f264b722d156ec219be62719b23
Tanya Tafolla <ttafolla@rclogin03.merced>
2024-01-22 10:49:04 -0800

    was missing a for-loop to iterate through methods

--- bat_files/finalrun/call_e4_1k_k.bat
+++ bat_files/finalrun/call_e4_1k_k.bat
@@ -7,6 +7,8 @@ methods=("case5_kiops.ini" "case6_kiops.ini" "galewsky_kiops.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
-for ((j = 0; j < 7; j++)); do
-     mpirun -np 1176 python3 ./main_gef.py config/test_files/epi4/${methods[${k}]}
+for ((k=0; k < $methodlen; k++)); do
+  for ((j = 0; j < 7; j++)); do
+       mpirun -np 1176 python3 ./main_gef.py config/test_files/epi4/${methods[${k}]}
+  done
 done
