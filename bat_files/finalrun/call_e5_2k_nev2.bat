30e74efa5b17ac7ea9901e0f2cbe04202a1bc2c5
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-26 16:39:20 -0800

    reduce to 5 calls

--- bat_files/finalrun/call_e5_2k_nev2.bat
+++ bat_files/finalrun/call_e5_2k_nev2.bat
@@ -8,7 +8,7 @@ methods=("epi5/case6_icwyne.ini" "epi5/galewsky_pmexne.ini" "epi5/galewsky_cwyne
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 2646 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
--- bat_files/finalrun/call_e6_2k_nev1.bat
+++ bat_files/finalrun/call_e6_2k_nev1.bat
@@ -8,7 +8,7 @@ methods=("epi6/case5_pmexne.ini" "epi6/case5_cwyne.ini" "epi6/case5_icwyne.ini" 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 2646 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
