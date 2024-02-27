21c78f26fc3c5ccd4b46446b114e61343d47387e
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-27 12:20:34 -0800

    reduced to 5 runs and group calls

--- bat_files/finalrun/call_epi_10k_1s.bat
+++ bat_files/finalrun/call_epi_10k_1s.bat
@@ -3,15 +3,13 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-#methods=("epi4/case5_pmex1s.ini" "epi4/case5_cwy1s.ini" "epi4/case5_icwy1s.ini" "epi4/case5_kiops.ini" "epi4/case6_pmex1s.ini" "epi4/case6_cwy1s.ini" "epi4/case6_icwy1s.ini" "epi4/case6_kiops.ini" "epi5/case5_pmex1s.ini" "epi5/case5_cwy1s.ini" "epi5/case5_icwy1s.ini" "epi5/case5_kiops.ini" "epi5/case6_pmex1s.ini" "epi5/case6_cwy1s.ini" "epi5/case6_icwy1s.ini" "epi5/case6_kiops.ini" "epi6/case5_pmex1s.ini" "epi6/case5_cwy1s.ini" "epi6/case5_icwy1s.ini" "epi6/case5_kiops.ini" "epi6/case6_pmex1s.ini" "epi6/case6_cwy1s.ini" "epi6/case6_icwy1s.ini" "epi6/case6_kiops.ini" "epi4/galewsky_pmex1s.ini" "epi4/galewsky_cwy1s.ini" "epi4/galewsky_icwy1s.ini" "epi4/galewksy_kiops.ini" "epi5/galewsky_pmex1s.ini" "epi5/galewsky_cwy1s.ini" "epi5/galewsky_icwy1s.ini" "galewsky_kiops.ini" "epi6/galewsky_pmex1s.ini" "epi6/galewsky_cwy1s.ini" "epi6/galewsky_icwy1s.ini" "epi6/galewsky_kiops.ini")
+methods=("epi4/case5_pmex1s.ini" "epi4/case5_cwy1s.ini" "epi4/case5_icwy1s.ini" "epi4/case5_kiops.ini" "epi4/case6_pmex1s.ini" "epi4/case6_cwy1s.ini" "epi4/case6_icwy1s.ini" "epi4/case6_kiops.ini" "epi4/galewsky_kiops.ini" "epi4/galewsky_pmex1s.ini" "epi4/galewsky_icwy1s.ini" "epi4/galewsky_cwy1s.ini" "epi4/galewsky_pmexne.ini" "epi4/galewsky_cwyne.ini" "epi4/galewsky_icwyne.ini" "epi4/case5_pmexne.ini" "epi4/case5_icwyne.ini" "epi4/case5_cwyne.ini" "epi4/case6_pmexne.ini" "epi4/case6_icwyne.ini" "epi4/case6_cwyne.ini" "epi5/galewsky_pmexne.ini" "epi5/galewsky_icwyne.ini" "epi5/galewsky_cwyne.ini" "epi5/case5_pmexne.ini" "epi5/case5_icwyne.ini" "epi5/case5_cwyne.ini" "epi5/case6_pmexne.ini" "epi5/case6_icwyne.ini" "epi5/case6_cwyne.ini" "epi5/galewsky_kiops.ini")
 
-
-methods=("epi4/case5_kiops.ini" "epi4/case6_kiops.ini" "epi5/case5_kiops.ini" "epi5/case6_kiops.ini" "epi6/case5_kiops.ini" "epi6/case6_kiops.ini" "epi4/galewksy_kiops.ini" "epi5/galewsky_pmex1s.ini" "epi5/galewsky_cwy1s.ini" "epi5/galewsky_icwy1s.ini" "epi5/galewsky_kiops.ini" "epi6/galewsky_kiops.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 10584 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
