f28dbd7178dabb6e046868f52dacf8d6bdc7fe23
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-26 16:24:54 -0800

    reduce to 5 runs

--- bat_files/finalrun/call_e6_864_1sv2.bat
+++ bat_files/finalrun/call_e6_864_1sv2.bat
@@ -8,7 +8,7 @@ methods=("epi6/galewsky_cwy1s.ini" "epi6/galewsky_pmex1s.ini" "epi6/galewsky_icw
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 864 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
--- bat_files/finalrun/call_e6_864_nev2.bat
+++ bat_files/finalrun/call_e6_864_nev2.bat
@@ -3,12 +3,12 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("epi6/galewsky_cwyne.ini" "epi6/galewsky_pmexne.ini" "epi6/galewsky_icwyne.ini" "epi6/galewsky_kiops.ini")
+methods=("epi6/galewsky_cwyne.ini" "epi6/galewsky_pmexne.ini" "epi6/galewsky_icwyne.ini" "epi6/galewsky_kiops.ini" "epi6/case5_kiops.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 864 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
