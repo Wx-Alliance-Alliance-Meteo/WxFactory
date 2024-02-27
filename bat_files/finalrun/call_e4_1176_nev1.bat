a3c947de52bffabe6ae549450fe3a868dca58387
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-26 16:30:03 -0800

    reduce to 5 calls

--- bat_files/finalrun/call_e4_1176_nev1.bat
+++ bat_files/finalrun/call_e4_1176_nev1.bat
@@ -3,12 +3,12 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("epi6/case5_pmexne.ini" "epi6/case5_icwyne.ini" "epi6/case6_pmexne.ini")
+methods=("epi4/case5_pmexne.ini" "epi4/case5_icwyne.ini" "epi4/case6_pmexne.ini" "epi4/case6_icwyne.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 1176 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
--- bat_files/finalrun/call_e4_1176_nev2.bat
+++ bat_files/finalrun/call_e4_1176_nev2.bat
@@ -3,13 +3,13 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-#methods=("epi4/case6_icwyne.ini" "epi4/galewsky_pmexne.ini" "epi4/galewsky_cwyne.ini" "epi4/galewsky_icwyne.ini")
-methods=("epi5/galewsky_kiops.ini" "epi6/case5_cwyne.ini" "epi6/case6_cwyne.ini")
+methods=("epi4/galewsky_pmexne.ini" "epi4/galewsky_cwyne.ini" "epi4/galewsky_icwyne.ini")
+#methods=("epi5/galewsky_kiops.ini" "epi6/case5_cwyne.ini" "epi6/case6_cwyne.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 1176 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
