58ead292306bbda43695b7a3878203a201882c4e
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-26 16:33:06 -0800

    redice to 5 calls

--- bat_files/finalrun/call_e5_1176_nev1.bat
+++ bat_files/finalrun/call_e5_1176_nev1.bat
@@ -8,7 +8,7 @@ methods=("epi5/case5_pmexne.ini" "epi5/case5_cwyne.ini" "epi5/case5_icwyne.ini" 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 1176 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
