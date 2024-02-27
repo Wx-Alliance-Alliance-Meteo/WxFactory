3a23fba19e3203d4d0a594bb59b786b880c930dc
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-27 11:42:58 -0800

    grouped calls to reduce files

--- bat_files/finalrun/call_e4_2k_1s.bat
+++ bat_files/finalrun/call_e4_2k_1s.bat
@@ -3,7 +3,7 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("epi4/case5_pmex1s.ini" "epi4/case5_cwy1s.ini" "epi4/case5_icwy1s.ini" "epi4/case5_kiops.ini" "epi4/case6_pmex1s.ini" "epi4/case6_cwy1s.ini" "epi4/case6_icwy1s.ini" "epi4/case6_kiops.ini" "epi4/galewsky_pmex1s.ini" "epi4/galewsky_cwy1s.ini" "epi4/galewsky_icwy1s.ini" "epi4/galewsky_kiops.ini")
+methods=("epi4/case5_pmex1s.ini" "epi4/case5_cwy1s.ini" "epi4/case5_icwy1s.ini" "epi4/case5_kiops.ini" "epi4/case6_pmex1s.ini" "epi4/case6_cwy1s.ini" "epi4/case6_icwy1s.ini" "epi4/case6_kiops.ini" "epi4/galewsky_pmex1s.ini" "epi4/galewsky_cwy1s.ini" "epi4/galewsky_icwy1s.ini" "epi4/galewsky_kiops.ini" "epi4/case6_pmexne.ini" "epi4/case6_pmexne.ini" "epi4/galewsky_pmexne.ini" "epi4/case6_icwyne.ini" "epi4/case6_cwyne.ini" "epi4/case5_cwyne.ini" "epi4/case5_cwyne.ini" "epi4/galewsky_cwyne.ini" "epi4/galewsky_icwyne.ini")
 
 #methods=("epi4/case5_icwy1s.ini")
 
