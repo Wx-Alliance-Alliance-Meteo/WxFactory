74b861cb7a841af58602e6edb155f1f230e0177f
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-27 18:42:35 -0800

    reduce to 5 calls

--- bat_files/call_1176_s3_1sv2.bat
+++ bat_files/call_1176_s3_1sv2.bat
@@ -8,7 +8,7 @@ methods=("case5_icwy1s.ini" "case6_icwy1s.ini" "galewsky_icwy1s.ini" "case6_kiop
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 1176 python3 ./main_gef.py config/test_files/srerk3/${methods[${k}]}
   done
 done
