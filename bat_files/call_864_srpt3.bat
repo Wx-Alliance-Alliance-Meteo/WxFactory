a9481bc13b527d6f155a35388f98bbc6ae362d4b
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-27 18:53:42 -0800

    reduce to 5 calls

--- bat_files/call_864_srpt3.bat
+++ bat_files/call_864_srpt3.bat
@@ -3,12 +3,12 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("srerk6/case6_icwy1s.ini" "srerk3/case5_cwyne.ini" "srerk3/case5_icwyne.ini" "srerk6/case5_icwy1s.ini")
+methods=("srerk3/case5_cwyne.ini" "srerk3/case5_icwyne.ini" "srerk3/case5_pmexne.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 864 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
--- bat_files/call_864_srpt4.bat
+++ bat_files/call_864_srpt4.bat
@@ -3,12 +3,12 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("srerk3/case6_pmexne.ini" "srerk3/case6_cwyne.ini" "srerk3/case6_icwyne.ini" "srerk6/galewsky_icwy1s.ini")
+methods=("srerk3/case6_pmexne.ini" "srerk3/case6_cwyne.ini" "srerk3/case6_icwyne.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 864 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
