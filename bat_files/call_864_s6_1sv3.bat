d83f75703bd72b2a33b36cc838edbdbfb4c0da1d
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-27 18:57:58 -0800

    reduce to 5 calls

--- bat_files/call_864_s6_1sv3.bat
+++ bat_files/call_864_s6_1sv3.bat
@@ -2,12 +2,12 @@
 cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
-methods=("srerk6/galewsky_pmex1s.ini" "srerk6/galewsky_kiops.ini" "srerk6/galewsky_cwy1s.ini" "srerk6/galewsky_icwyne.ini")
+methods=("srerk6/galewsky_pmex1s.ini" "srerk6/galewsky_kiops.ini" "srerk6/galewsky_cwy1s.ini" "srerk6/galewsky_icwy1s.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 864 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
