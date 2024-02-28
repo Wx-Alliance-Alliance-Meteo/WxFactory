8069628ba93c1846faa9aff8fd69b6304b1d69a4
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-27 18:31:52 -0800

    reduced to 5 calls

--- bat_files/call_2646_nesr6v1.bat
+++ bat_files/call_2646_nesr6v1.bat
@@ -8,7 +8,7 @@ methods=("case5_cwyne.ini" "case5_icwyne.ini" "case5_pmexne.ini" "case6_cwyne.in
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 2646 python3 ./main_gef.py config/test_files/srerk6/${methods[${k}]}
   done
 done
--- bat_files/call_2646_s6_1sv1.bat
+++ bat_files/call_2646_s6_1sv1.bat
@@ -8,7 +8,7 @@ methods=("case5_kiops.ini" "case6_kiops.ini" "galewsky_kiops.ini" "case5_pmex1s.
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 2646 python3 ./main_gef.py config/test_files/srerk6/${methods[${k}]}
   done
 done
