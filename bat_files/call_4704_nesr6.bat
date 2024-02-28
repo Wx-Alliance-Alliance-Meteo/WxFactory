ef56d43309abe21ae6c14823f7d8468060c331fb
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-27 18:18:34 -0800

    reduce to 5 runs for 4k s6

--- bat_files/call_4704_nesr6.bat
+++ bat_files/call_4704_nesr6.bat
@@ -3,12 +3,12 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("srerk6/case5_pmexne.ini" "srerk6/case6_pmexne.ini" "srerk6/galewsky_pmexne.ini" "srerk6/galewsky_icwyne.ini" "srerk6/case6_icwyne.ini" "srerk6/case5_icwyne.ini" "srerk6/case6_cwyne.ini" "srerk6/case5_cwyne.ini" "srerk6/galewsky_cwyne.ini")
+methods=("srerk6/galewsky_kiops.ini" "srerk6/galewsky_pmex1s.ini" "srerk6/galewsky_icwy1s.ini" "srerk6/galewsky_cwy1s.ini" "srerk6/galewsky_pmexne.ini" "srerk6/galewsky_icwyne.ini" "srerk6/galewsky_cwyne.ini" "srerk6/case6_cwyne.ini" "srerk6/case6_icwyne.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 4704 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
