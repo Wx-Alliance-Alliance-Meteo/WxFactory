31f2bb96da035fd86273ab7b78c7d36d97f69fa4
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-27 18:39:05 -0800

    reduce to 5 calls

--- bat_files/call_1176_s6_nev3.bat
+++ bat_files/call_1176_s6_nev3.bat
@@ -8,7 +8,7 @@ methods=("case5_cwyne.ini" "case6_cwyne.ini" "galewsky_cwyne.ini")
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 1176 python3 ./main_gef.py config/test_files/srerk6/${methods[${k}]}
   done
 done
