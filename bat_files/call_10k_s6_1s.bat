a03a94adf868ee1b1a27aea92bb4fcf221ee22a1
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-02-27 17:58:06 -0800

    rearranged calls and reduced to 5 runs

--- bat_files/call_10k_s6_1s.bat
+++ bat_files/call_10k_s6_1s.bat
@@ -3,12 +3,12 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("case5_kiops.ini" "case5_pmex1s.ini" "case5_cwy1s.ini" "case5_icwy1s.ini" "case6_kiops.ini" "case6_pmex1s.ini" "case6_cwy1s.ini" "case6_icwy1s.ini" "galewsky_kiops.ini" "galewsky_pmex1s.ini" "galewsky_cwy1s.ini" "galewsky_icwy1s.ini" "galewsky_cwyne.ini")
+methods=("case5_kiops.ini" "case5_pmex1s.ini" "case5_cwy1s.ini" "case5_icwy1s.ini" "case5_pmexne.ini" "case5_icwyne.ini" "case5_cwyne.ini" "case6_pmex1s.ini" "case6_icwy1s.ini" "case6_cwy1s.ini" "case6_kiops.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 10584 python3 ./main_gef.py config/test_files/srerk6/${methods[${k}]}
   done
 done
--- bat_files/call_10k_s6_ne.bat
+++ bat_files/call_10k_s6_ne.bat
@@ -3,12 +3,12 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("srerk6/case5_pmexne.ini" "srerk6/case5_cwyne.ini" "srerk6/case5_icwyne.ini" "srerk6/case6_pmexne.ini" "srerk6/case6_cwyne.ini" "srerk6/case6_icwyne.ini" "srerk6/galewsky_pmexne.ini" "srerk6/galewsky_icwyne.ini" "srerk6/galewsky_cwyne.ini" "srerk3/galewsky_kiops.ini" "srerk3/case5_kiops.ini" "srerk3/case6_kiops.ini")
+methods=("srerk6/galewsky_pmexne.ini" "srerk6/galewsky_icwyne.ini" "srerk6/galewsky_cwyne.ini" "srerk6/galewsky_kiops.ini" "srerk6/galewsky_pmex1s.ini" "srerk6/galewsky_cwy1s.ini" "srerk6/galewsky_icwy1s.ini" "srerk6/case6_pmexne.ini" "srerk6/case6_icwyne.ini" "srerk6/case6_cwyne.ini" "srerk6/case6_kiops.ini")
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
 for ((k=0; k < $methodlen; k++)); do
-   for ((j = 0; j < 7; j++)); do
+   for ((j = 0; j < 5; j++)); do
      mpirun -np 10584 python3 ./main_gef.py config/test_files/${methods[${k}]}
   done
 done
