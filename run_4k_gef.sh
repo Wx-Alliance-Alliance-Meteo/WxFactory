2e7511acd6da928e260b1a2a04c4ab76911410f1
Tanya Tafolla <ttafolla@rclogin01.cluster>
2024-03-01 13:52:10 -0800

    rerunning the missing test

--- run_4k_gef.sh
+++ run_4k_gef.sh
@@ -1,36 +1,3 @@
-#----------------------4704---------------------------------
-#---s6---
-ord_soumet -cpus 4704                                \
-           -w 90                                    \
-           -jn gef_4704                         \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/call_4704_nesr6.bat      \
-           -listing $(pwd)/listings
-
-ord_soumet -cpus 4704                                \
-           -w 180                                    \
-           -jn gef_4704                         \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/call_4704_s6_1s.bat      \
-           -listing $(pwd)/listings
-#srerk3 
-ord_soumet -cpus 4704                                \
-           -w 180                                    \
-           -jn gef_4704                         \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/call_4704_s3_1s.bat      \
-           -listing $(pwd)/listings
-
-ord_soumet -cpus 4704                                \
-           -w 180                                    \
-           -jn gef_4704                         \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/call_4704_nesr3.bat      \
-           -listing $(pwd)/listings
 
 #--epi---
 ord_soumet -cpus 4704                                \
@@ -38,7 +5,7 @@ ord_soumet -cpus 4704                                \
            -jn gef_4704                         \
            -mpi                                      \
            -share e                                  \
-           -jobfile bat_files/final_run/call_epi_4k_1sv1.bat      \
+           -jobfile bat_files/finalrun/call_epi_4k_1sv1.bat      \
            -listing $(pwd)/listings
 
 ord_soumet -cpus 4704                                \
@@ -46,7 +13,7 @@ ord_soumet -cpus 4704                                \
            -jn gef_4704                         \
            -mpi                                      \
            -share e                                  \
-           -jobfile bat_files/final_run/call_epi_4k_1sv2.bat      \
+           -jobfile bat_files/finalrun/call_epi_4k_1sv2.bat      \
            -listing $(pwd)/listings
 
 ord_soumet -cpus 4704                                \
@@ -54,7 +21,7 @@ ord_soumet -cpus 4704                                \
            -jn gef_4704                         \
            -mpi                                      \
            -share e                                  \
-           -jobfile bat_files/final_run/call_epi_4k_nev1.bat      \
+           -jobfile bat_files/finalrun/call_epi_4k_nev1.bat      \
            -listing $(pwd)/listings
 
 
