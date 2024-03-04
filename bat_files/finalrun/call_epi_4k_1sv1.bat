d1cb3fa843b9dbb8b16f89eaed6e7949519c1030
Tanya Tafolla <ttafolla@rclogin03.merced>
2024-03-04 14:01:00 -0800

    fixing typo in kiops call for case 6

--- bat_files/finalrun/call_epi_4k_1sv1.bat
+++ bat_files/finalrun/call_epi_4k_1sv1.bat
@@ -3,7 +3,11 @@ cd /home/vma000/code/gef_tanya
 . ./load_env.sh
 
 
-methods=("epi4/case5_kiops.ini" "epi4/case6_kiops." "epi4/galewsky_kiops.ini" "epi4/case5_pmex1s.ini" "epi4/case6_pmex1s.ini" "epi4/galewsky_pmex1s.ini" "epi4/case5_icwy1s.ini" "epi4/case6_icwy1s.ini" "epi4/galewsky_icwy1s.ini" "epi4/case5_cwy1s.ini" "epi4/case6_cwy1s.ini" "epi4/galewsky_cwy1s.ini" "epi4/case5_pmexne.ini" "epi4/case6_pmexne.ini" "epi4/galewsky_pmexne.ini" "epi4/case5_icwyne.ini" "epi4/case6_icwyne.ini" "epi4/galewsky_icwyne.ini" "epi4/case6_cwyne.ini" "epi4/case5_cwyne.ini" "epi4/galewsky_cwyne.ini")
+#methods=("epi4/case5_kiops.ini" "epi4/case6_kiops." "epi4/galewsky_kiops.ini" "epi4/case5_pmex1s.ini" "epi4/case6_pmex1s.ini" "epi4/galewsky_pmex1s.ini" "epi4/case5_icwy1s.ini" "epi4/case6_icwy1s.ini" "epi4/galewsky_icwy1s.ini" "epi4/case5_cwy1s.ini" "epi4/case6_cwy1s.ini" "epi4/galewsky_cwy1s.ini" "epi4/case5_pmexne.ini" "epi4/case6_pmexne.ini" "epi4/galewsky_pmexne.ini" "epi4/case5_icwyne.ini" "epi4/case6_icwyne.ini" "epi4/galewsky_icwyne.ini" "epi4/case6_cwyne.ini" "epi4/case5_cwyne.ini" "epi4/galewsky_cwyne.ini")
+
+
+methods=("epi4/case6_kiops.ini" "epi5/case6_kiops.ini" "epi6/case6_kiops.ini")
+
 
 methodlen=${#methods[@]}
 #looping through each case and running it 7 times
--- run_4k_gef.sh
+++ run_4k_gef.sh
@@ -1,28 +1,10 @@
 
 #--epi---
 ord_soumet -cpus 4704                                \
-           -w 180                                    \
+           -w 40                                    \
            -jn gef_4704                         \
            -mpi                                      \
            -share e                                  \
            -jobfile bat_files/finalrun/call_epi_4k_1sv1.bat      \
            -listing $(pwd)/listings
-
-ord_soumet -cpus 4704                                \
-           -w 180                                    \
-           -jn gef_4704                         \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/finalrun/call_epi_4k_1sv2.bat      \
-           -listing $(pwd)/listings
-
-ord_soumet -cpus 4704                                \
-           -w 180                                    \
-           -jn gef_4704                         \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/finalrun/call_epi_4k_nev1.bat      \
-           -listing $(pwd)/listings
-
-
 
