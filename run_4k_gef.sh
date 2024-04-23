14dd7b0354bfcf4e4ccac12b3e42e3894d702836
Tanya Tafolla <ttafolla@rclogin03.merced>
2024-04-23 06:46:07 -0700

    running missing 4k and 1k files

--- run_4k_gef.sh
+++ run_4k_gef.sh
@@ -1,37 +1,16 @@
 
-#--epi---
-ord_soumet -cpus 4704                                \
-           -w 180                                    \
-           -jn gef_4704                         \
+ord_soumet -cpus 1176                                \
+           -w 60                                    \
+           -jn gef_1176                         \
            -mpi                                      \
            -share e                                  \
-           -jobfile bat_files/finalrun/call_epi_4k_nev1.bat      \
+           -jobfile bat_files/finalrun/call_e5_1176_nev2.bat      \
            -listing $(pwd)/listings
-
-ord_soumet -cpus 4704                                \
-           -w 180                                    \
-           -jn gef_4704                         \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/finalrun/call_epi_4k_nev2.bat      \
-           -listing $(pwd)/listings
-
 #---srerk---
 ord_soumet -cpus 4704                                \
-           -w 180                                    \
-           -jn gef_4704                         \
+           -w 90                                     \
+           -jn gef_4704                              \
            -mpi                                      \
            -share e                                  \
-           -jobfile bat_files/call_4704_nesr3.bat      \
+           -jobfile bat_files/call_4704_nesr3.bat    \
            -listing $(pwd)/listings
-
-ord_soumet -cpus 4704                                \
-           -w 180                                    \
-           -jn gef_4704                         \
-           -mpi                                      \
-           -share e                                  \
-           -jobfile bat_files/call_4704_nesr6.bat      \
-           -listing $(pwd)/listings
-
-
-
