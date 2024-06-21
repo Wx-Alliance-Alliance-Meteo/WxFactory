51751d4e30a7c5581ad340b277124fa7d8a2cd1a
Vincent Magnoux <vincent.magnoux@ec.gc.ca>
2024-06-21 07:11:23 -0700

    Configure MPI launch to assign more cores to each process

--- wx.sh
+++ wx.sh
@@ -7,8 +7,8 @@
 #SBATCH --output=hello.out
 #SBATCH --partition=gpu
 #SBATCH --time=00:30:00
-#SBATCH --ntasks=6
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=16
 #SBATCH --nodes=1
 #SBATCH --gpus=6
 #SBATCH --mem-per-cpu=8000M
@@ -22,7 +22,7 @@ module load nvhpc-pmix/24.3
 . /home/pr-h6bcfd5d/wx_env/bin/activate
 module load nsight-systems/2023.3
 
-cd ~/WxFactory
+cd ~/wx_factory
 
 config_file=config/dcmip31_cuda_small.ini
 [ $# -ge 1 ] && config_file=${1}
@@ -30,6 +30,15 @@ config_file=config/dcmip31_cuda_small.ini
 echo "Running WxFactory with config ${config_file}"
 pwd
 
-#nsys profile mpirun ./main_gef.py ${config_file}
-mpirun nsys profile -o gef.%q{OMPI_COMM_WORLD_RANK} -f true -t cuda,nvtx ./main_gef.py ${config_file}
+export MELLANOX_VISIBLE_DEVICES=0
+#export UCX_UD_TIMEOUT=120s
+#export UCX_TLS=^ud
+export UCX_TLS=^ud,ud:aux
+#mpirun -n 6 --map-by ppr:4:socket:PE=16 ./main_gef.py ${config_file}
+mpirun -n 6 --map-by ppr:4:socket:PE=16 nsys profile -o gef_tmp.%q{OMPI_COMM_WORLD_RANK} -f true -t cuda,nvtx ./main_gef.py ${config_file}
+#mpirun -np 6  --tag-output  --map-by ppr:4:socket:PE=16    numactl --show 
+#mpirun -np 1  --tag-output  --map-by ppr:4:socket:PE=16    numactl --show 
+#nvidia-smi topo -m
+
+#lscpu
 
