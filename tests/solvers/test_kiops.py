a805fa4001ba25d578f3d6107dcd92215bdd144b
Linden Brochu <lindenb477@hotmail.com>
2024-09-16 19:26:33 +0000

    Create test to compare KIOPS and PMEX with tolerance

--- tests/solvers/test_kiops.py
+++ tests/solvers/test_kiops.py
@@ -1,18 +1,17 @@
-from solvers.pmex import pmex
+from solvers.kiops import kiops
 import random
 from numpy import ndarray
 
 import cuda_test
 
-class PmexTestCases(cuda_test.CudaTestCases):
-    pmex_mmax: int
+class KiopsTestCases(cuda_test.CudaTestCases):
     tolerance: float
-    cpu_gpu_tolenrance: float
+    cpu_to_gpu_tolerence: float
     rand: random.Random
 
     cpu_matrix: ndarray
     gpu_matrix: ndarray
-    
+
     def setUp(self) -> None:
         super().setUp()
         seed: int = 5646459
@@ -20,10 +19,10 @@ class PmexTestCases(cuda_test.CudaTestCases):
         rand_min: float = -1000.0
         rand_max: float = 1000.0
 
-        self.show_debug_print = False
+        self.show_debug_print = True
 
         self.tolerance = 1e-7
-        self.cpu_gpu_tolenrance = 1e-12
+        self.cpu_gpu_tolenrance = 1e-10
         self.rand = random.Random(seed)
 
         self.cpu_matrix: ndarray = self.cpu_device.xp.zeros((initial_matrix_size, initial_matrix_size), dtype=float)
@@ -34,21 +33,20 @@ class PmexTestCases(cuda_test.CudaTestCases):
                 nb: float = self.rand.uniform(rand_min, rand_max)
                 self.cpu_matrix[it1, it2] = nb
                 self.gpu_matrix[it1, it2] = nb
-
+    
     def test_compare_cpu_to_gpu(self):
         def matvec_handle(v: ndarray) -> ndarray: return v
 
         w1: ndarray
         w2: ndarray
 
-        w1, stats1 = pmex([1.0], matvec_handle, self.cpu_matrix, self.tolerance, device=self.cpu_device)
-
-        w2, stats2 = pmex([1.0], matvec_handle, self.gpu_matrix, self.tolerance, device=self.gpu_device)
+        w1, stats1 = kiops([1.], matvec_handle, self.cpu_matrix, self.tolerance, device=self.cpu_device)
+        w2, stats2 = kiops([1.], matvec_handle, self.gpu_matrix, self.tolerance, device=self.gpu_device)
 
         shape: tuple[int, int] = w1.shape
 
-        self.assertEqual(len(w1.shape), 2, 'Pmex didn\'t return a matrix with the cpu device')
-        self.assertEqual(len(w2.shape), 2, 'Pmex didn\'t return a matrix with the gpu device')
+        self.assertEqual(len(w1.shape), 2, 'Kiops didn\'t return a matrix with the cpu device')
+        self.assertEqual(len(w2.shape), 2, 'Kiops didn\'t return a matrix with the gpu device')
 
         self.failIf(not (w2.shape[0] == shape[0] and w2.shape[1] == shape[1]), 'Both matrix should be the same size')
 
@@ -56,16 +54,15 @@ class PmexTestCases(cuda_test.CudaTestCases):
         w2_value: float = self.gpu_device.xp.sum(w2).item()
 
         abs_diff: float = abs(w1_value - w2_value) / (shape[0] * shape[1])
-        
+
         relative_diff_w1: float = abs(abs_diff / w1_value)
         relative_diff_w2: float = abs(abs_diff / w2_value)
 
         if self.show_debug_print:
-            c_name = 'PmexTestCases'
+            c_name = 'KiopsTestCases'
             m_name = 'test_compare_cpu_to_gpu'
             print(f'In {c_name}.{m_name}, absolute difference is {abs_diff}')
             print(f'In {c_name}.{m_name}, relative difference from cpu is {relative_diff_w1} and relative difference from gpu is {relative_diff_w2}')
 
-        self.assertLessEqual(relative_diff_w1, self.cpu_gpu_tolenrance, 'Pmex didn\'t give a close result to the cpu value')
-        self.assertLessEqual(relative_diff_w2, self.cpu_gpu_tolenrance, 'Pmex didn\'t give a close result to the gpu value')
-        
+        self.assertLessEqual(relative_diff_w1, self.cpu_gpu_tolenrance, f'Kiops didn\'t give a close result to the cpu value')
+        self.assertLessEqual(relative_diff_w2, self.cpu_gpu_tolenrance, f'Kiops didn\'t give a close result to the gpu value')
