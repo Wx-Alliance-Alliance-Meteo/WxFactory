45f78d9ae9aea5a4f64a318c3fcbccaa5765b7d8
Linden Brochu <lindenb477@hotmail.com>
2024-09-16 15:20:06 +0000

    Test the PMEX algorithm

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -7,7 +7,16 @@ import unittest
 main_project_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
 sys.path.append(main_project_dir)
 
-from process_topology import ProcessTopologyTest
+from tests.process_topology import ProcessTopologyTest
+from tests.solvers.test_pmex import PmexTestCases
+from tests.solvers.test_kiops import KiopsTestCases
+
+def load_tests():
+    suite = unittest.TestSuite()
+    suite.addTest(PmexTestCases('test_compare_cpu_to_gpu'))
+    suite.addTest(KiopsTestCases('test_compare_cpu_to_gpu'))
+    return suite
 
 if __name__ == '__main__':
-    unittest.main()
+    runner = unittest.TextTestRunner()
+    runner.run(load_tests())
