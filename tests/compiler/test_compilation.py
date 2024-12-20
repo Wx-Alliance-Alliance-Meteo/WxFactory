097c4f52073a4f360c6b3b8077ef149096c06ddf
Linden Brochu <lindenb477@hotmail.com>
2024-12-20 16:19:42 +0000

    Add setuptools

--- tests/compiler/test_compilation.py
+++ tests/compiler/test_compilation.py
@@ -2,6 +2,7 @@ import unittest
 import compiler.compile_utils
 import os
 
+
 class CompilationTestCases(unittest.TestCase):
     def setUp(self):
         super().setUp()
@@ -21,22 +22,19 @@ class CompilationTestCases(unittest.TestCase):
         import lib.pde.interface_c as interface_c
 
     def test_cuda_kernels_compilation(self):
-        import wx_cupy
-        wx_cupy.init_wx_cupy()
-        if not wx_cupy.cuda_avail:
-            self.skipTest("Cuda not available")
-
         lib_path = os.path.join("lib", "pde", "interface_cuda.so")
         hash_path = os.path.join("lib", "cuda", "hash")
-        
+
         self.assertFalse(os.path.exists(lib_path))
         self.assertFalse(os.path.exists(hash_path))
+
         compiler.compile_utils.compile("cuda")
+
         self.assertTrue(os.path.exists(lib_path))
         self.assertTrue(os.path.exists(hash_path))
 
         import lib.pde.interface_cuda as interface_cuda
-    
+
     def test_cpp_compilation_twice(self):
         lib_path = os.path.join("lib", "pde", "interface_c.so")
         hash_path = os.path.join("lib", "cpp", "hash")
