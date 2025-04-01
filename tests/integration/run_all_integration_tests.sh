67ae78d4aeb1073ce94c2014a237a56c5b7ea396
Vincent Magnoux <vincent.magnoux@ec.gc.ca>
2025-04-01 15:25:27 +0000

    Added a small DCMIP 2-1 test case to integration tests

--- tests/integration/run_all_integration_tests.sh
+++ tests/integration/run_all_integration_tests.sh
@@ -2,3 +2,4 @@
 # mpirun -n 2 python tests/integration/run_integration_tests.py small_cartesian2d_problem || exit -1
 ./tests/integration/run_integration_tests.py small_cartesian2d_problem || exit -1
 mpirun -n 24 ./tests/integration/run_integration_tests.py small_sw_case5 || exit -1
+mpirun -n 54 ./tests/integration/run_integration_tests.py small_dcmip21 || exit -1
