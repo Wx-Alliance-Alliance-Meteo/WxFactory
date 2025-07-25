7c76c4602ab80c2e0b8c0b42af6eef771d957d61
Vincent Magnoux <vincent.magnoux@ec.gc.ca>
2025-07-25 15:22:36 +0000

    Test improvements:
    - Fixed compilation test
    - Variable to more easily run on slurm systems

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -3,8 +3,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 WX_DIR=${SCRIPT_DIR}/..
 
+export MPIRUN=${MPIRUN:-mpirun}
+
 cd ${WX_DIR}
 
-${WX_DIR}/tests/unit/run_tests.py || exit -1
-mpirun -n 24 ${WX_DIR}/tests/unit/run_mpi_tests.py || exit -1
-${WX_DIR}/tests/integration/run_all_integration_tests.sh || exit -1
+${MPIRUN} -n 1 ${WX_DIR}/tests/unit/run_tests.py || exit -1
+${MPIRUN} -n 24 ${WX_DIR}/tests/unit/run_mpi_tests.py || exit -1
+${MPIRUN} -n 1 ${WX_DIR}/tests/integration/run_all_integration_tests.sh || exit -1
