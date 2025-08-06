8413972e970d1cadf4f986cde18b6dfeea713c62
Linden Brochu <linden.brochu@ec.gc.ca>
2025-08-06 09:45:24 -0400

    fix super call

--- wx_factory/integrators/neural.py
+++ wx_factory/integrators/neural.py
@@ -4,7 +4,7 @@ from .integrator import Integrator
 
 class Neural(Integrator):
     def __init__(self, param: Configuration):
-          super().__init__(self, param, preconditioner=None)
+          super().__init__(param, preconditioner=None)
 
     def __step__(self, Q, dt):
         return Q
