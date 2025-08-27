b04cdd51f4612af2e241ef7f193ee1964e6a83b7
Vincent Magnoux <vincent.magnoux@ec.gc.ca>
2025-08-27 13:56:57 +0000

    [tests] Added testing info

--- scripts/generate_potential_temperature_plot.py
+++ scripts/generate_potential_temperature_plot.py
@@ -14,20 +14,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import output
 from mpi4py import MPI
+
 np.set_printoptions(linewidth=sys.maxsize)
 
 
-data = nc.Dataset('./tests/data/temp/out.nc', 'r')
-_, T0 = output.InputManager.read_config_from_save_file('./tests/data/temp/old/state_vector_47170bb8616a.00000000.npy', MPI.COMM_WORLD)
+data = nc.Dataset("./tests/data/temp/out.nc", "r")
+_, T0 = output.InputManager.read_config_from_save_file(
+    "./tests/data/temp/old/state_vector_47170bb8616a.00000000.npy", MPI.COMM_WORLD
+)
 
 # Setup figure
 fig = plt.figure(figsize=(10, 3))
 
-times = data['time']
-θ = data['theta']
-elev = data['elev']
-lons = data['lons']
-lats = data['lats']
+times = data["time"]
+θ = data["theta"]
+print(f"theta = {θ}", flush=True)
+elev = data["elev"]
+lons = data["lons"]
+lats = data["lats"]
 (ntime, npanels, nk, nj, ni) = θ.shape
 
 # Define your data or variables (lons, elev, θ, times, nj, etc.)
@@ -39,38 +43,38 @@ yrow = nj - 1
 dthetamin = np.amin(θ[ts, :, :, yrow, :] - T0_theta[:, :, yrow, :])
 dthetamax = np.amax(θ[ts, :, :, yrow, :] - T0_theta[:, :, yrow, :])
 abstheta = max(abs(dthetamin), abs(dthetamax))
-#bstheta = min(abstheta, 3)
+# bstheta = min(abstheta, 3)
 
 
 handles = []
 all_lons = []
 all_elev = []
 all_theta = []
-for panel in [0,1,2,3]:
-    llons = np.mod(0+lons[panel,yrow,:],360)-0
-    
-    all_lons.append(np.tile(llons,(elev.shape[1],1)))
-    all_elev.append(elev[panel,:,yrow,:]/1e3)
-    all_theta.append(θ[ts,panel,:,yrow,:]-T0_theta[panel,:,yrow,:])
+for panel in [0, 1, 2, 3]:
+    llons = np.mod(0 + lons[panel, yrow, :], 360) - 0
 
-combined_lons = np.concatenate(all_lons,axis=1)
-combined_elev = np.concatenate(all_elev,axis=1)
-combined_theta = np.concatenate(all_theta,axis=1)
+    all_lons.append(np.tile(llons, (elev.shape[1], 1)))
+    all_elev.append(elev[panel, :, yrow, :] / 1e3)
+    all_theta.append(θ[ts, panel, :, yrow, :] - T0_theta[panel, :, yrow, :])
+
+combined_lons = np.concatenate(all_lons, axis=1)
+combined_elev = np.concatenate(all_elev, axis=1)
+combined_theta = np.concatenate(all_theta, axis=1)
 
 lin_space = np.linspace(dthetamin, dthetamax, 10)
 labels = np.linspace(dthetamin, dthetamax, 10)
-c = plt.contourf(combined_lons,combined_elev,combined_theta,levels=lin_space,cmap='summer')
+c = plt.contourf(combined_lons, combined_elev, combined_theta, levels=lin_space, cmap="summer")
 cb = plt.colorbar(shrink=1)
-cont = plt.contour(combined_lons,combined_elev,combined_theta,levels=labels, colors=['black'], linewidths= 0.8)
+cont = plt.contour(combined_lons, combined_elev, combined_theta, levels=labels, colors=["black"], linewidths=0.8)
 # cb.ax.set_ylabel('W',fontsize=14)
 
 cb.set_ticks(ticks=labels, labels=[str(pos) for pos in labels])
-cb.ax.set_ylabel('Δθ',fontsize=14)
-plt.gca().set_xlabel('\phi')
-plt.gca().set_xticks([0,90,180,270,360])
-plt.gca().set_title('t = 3600s')
-plt.gca().tick_params(axis='x', labelsize=14)
-plt.gca().tick_params(axis='y', labelsize=14)
-plt.gca().set_ylabel('H (km)', fontsize=14)
-plt.gca().set_xlabel('Longitude', fontsize=14)
-plt.savefig('DCMIP_31_3600', dpi=300, bbox_inches='tight')
+cb.ax.set_ylabel("Δθ", fontsize=14)
+plt.gca().set_xlabel("\phi")
+plt.gca().set_xticks([0, 90, 180, 270, 360])
+plt.gca().set_title("t = 3600s")
+plt.gca().tick_params(axis="x", labelsize=14)
+plt.gca().tick_params(axis="y", labelsize=14)
+plt.gca().set_ylabel("H (km)", fontsize=14)
+plt.gca().set_xlabel("Longitude", fontsize=14)
+plt.savefig("DCMIP_31_3600", dpi=300, bbox_inches="tight")
--- tests/readme.md
+++ tests/readme.md
@@ -91,3 +91,19 @@ There are no conventions yet, but here are our recommendations :
 * Each test case should start with `test_{whatever you are testing}`
 * If possible, use already created entrypoints
 * To add an integration test, add a line in the `run_all_integration_tests.sh`
+
+## Validation
+
+### Pour les configs dans tests/data/integration/dcmip{21,31}
+* run WxFactory on this config (up to ~40 min)
+* mpirun -n 6 ./scripts/convert_to_old.py [pas d'input pour l'instant, mais ça va être l'output de la config qui vient de rouler, timestep 0]
+* ./scripts/generate potential temperature [pas d'input pour l'isntant, mais on veut netcfd + state vector (ancient layout)]
+* comparer avec Figure 8
+* ./script generate hovmoller [même input que l'autre]
+* comparer avec Fig 9
+
+### TODO
+* une fois que c'est fait, rouler les *gros* tests dans tests/data/validation
+* Vérifier que c,est les mêmes résultats que ceux dans tests/data/integration/dcmip31
+* Stocker le résultat dans le dépôt
+* Vérifier que ça roule sur GPU
