604043bc5f5ad675c673931b1d461f8721365c73
Stéphane Gaudreault <stephane.gaudreault@gmail.com>
2026-05-27 10:56:22 -0400

    Fix mutable default argument in SolverInfo

--- wx_factory/solvers/solver_info.py
+++ wx_factory/solvers/solver_info.py
@@ -1,11 +1,11 @@
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 
 class SolverInfo:
     def __init__(
-        self, flag: int = 0, time: float = 0.0, total_num_it: int = 0, iterations: List[Tuple[float, float, float]] = []
+        self, flag: int = 0, time: float = 0.0, total_num_it: int = 0, iterations: Optional[List[Tuple[float, float, float]]] = None
     ) -> None:
         self.flag = flag
         self.time = time
         self.total_num_it = total_num_it
-        self.iterations = iterations
+        self.iterations = iterations if iterations is not None else []
