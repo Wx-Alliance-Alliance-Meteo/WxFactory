78800455f10edee03b30740061efbce620340cfe
Vincent Magnoux <vincent.magnoux@ec.gc.ca>
2026-07-21 18:39:55 +0000

    [tests] Fixed scripts for running tests

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -3,11 +3,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 WX_DIR=${SCRIPT_DIR}/..
 
-export MPIRUN=${MPIRUN:-mpirun}
-export PYTHON=$(which python3)
+export PYTHONPATH=${PYTHONPATH}:${WX_DIR}
 
 cd ${WX_DIR}
 
-${MPIRUN} -n 1 ${PYTHON} ${WX_DIR}/tests/unit/run_tests.py || exit -1
-${MPIRUN} -n 24 ${PYTHON} ${WX_DIR}/tests/unit/run_mpi_tests.py || exit -1
+${WX_DIR}/scripts/run.sh -n 1 ${WX_DIR}/tests/unit/run_tests.py || exit -1
+${WX_DIR}/scripts/run.sh -n 24 ${PYTHON} ${WX_DIR}/tests/unit/run_mpi_tests.py || exit -1
 ${WX_DIR}/tests/integration/run_all_integration_tests.sh || exit -1
